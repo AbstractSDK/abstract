4d1b7605eb44ea37c26c374d00016e2de3be3981
adairrr <32375605+adairrr@users.noreply.github.com>
2022-07-04 12:03:00 +0200

    Generate the subscription contract schema (#42)

--- contracts/modules/add-ons/subscription/examples/schema.rs
+++ contracts/modules/add-ons/subscription/examples/schema.rs
@@ -1,10 +1,12 @@
 use std::env::current_dir;
 use std::fs::create_dir_all;
 
-use cosmwasm_schema::{export_schema, remove_schemas, schema_for};
+use cosmwasm_schema::{export_schema, remove_schemas, schema_for, export_schema_with_title};
 
 use abstract_add_on::state::AddOnState;
-use abstract_os::modules::add_ons::subscription::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
+use abstract_os::modules::add_ons::subscription::msg::{ExecuteMsg, InstantiateMsg, QueryMsg, StateResponse, ConfigResponse, ContributorStateResponse, SubscriberStateResponse, SubscriptionFeeResponse};
+use cosmwasm_std::Binary;
+use cw_asset::AssetInfo;
 
 fn main() {
     let mut out_dir = current_dir().unwrap();
@@ -12,8 +14,25 @@ fn main() {
     create_dir_all(&out_dir).unwrap();
     remove_schemas(&out_dir).unwrap();
 
+
     export_schema(&schema_for!(InstantiateMsg), &out_dir);
     export_schema(&schema_for!(ExecuteMsg), &out_dir);
     export_schema(&schema_for!(QueryMsg), &out_dir);
     export_schema(&schema_for!(AddOnState), &out_dir);
+    export_schema(&schema_for!(StateResponse), &out_dir);
+    export_schema(&schema_for!(ContributorStateResponse), &out_dir);
+    export_schema(&schema_for!(SubscriberStateResponse), &out_dir);
+    export_schema(&schema_for!(ConfigResponse), &out_dir);
+
+    export_schema_with_title(
+        &schema_for!(AssetInfo),
+        &out_dir,
+        "AssetInfoBase_for_Addr",
+    );
+
+    export_schema_with_title(
+        &schema_for!(SubscriptionFeeResponse),
+         &out_dir,
+        "FeeResponse"
+    );
 }
