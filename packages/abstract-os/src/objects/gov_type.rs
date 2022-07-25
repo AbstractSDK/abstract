3b68cd85349c4203190ae18a2ae58e3ab141e498
CyberHoward <88450409+CyberHoward@users.noreply.github.com>
2022-07-25 14:57:54 +0200

    remove multisig gov type (#61)

--- packages/abstract-os/src/objects/gov_type.rs
+++ packages/abstract-os/src/objects/gov_type.rs
@@ -12,15 +12,6 @@ pub enum GovernanceDetails {
         /// The monarch's address
         monarch: String,
     },
-    /// Fixed multi-sig governance
-    MultiSignature {
-        /// Number of signatures
-        total_members: u8,
-        /// Minimum amounts of votes for a proposal to pass
-        threshold_votes: u8,
-        /// Member addresses, must be of length total_members
-        members: Vec<String>,
-    },
     /// An external governance source
     External {
         /// The external contract address
@@ -34,11 +25,6 @@ impl ToString for GovernanceDetails {
     fn to_string(&self) -> String {
         match self {
             GovernanceDetails::Monarchy { monarch: _ } => "monarchy".to_string(),
-            GovernanceDetails::MultiSignature {
-                total_members: _,
-                threshold_votes: _,
-                members: _,
-            } => "multisig".to_string(),
             GovernanceDetails::External {
                 governance_address: _,
                 governance_type,
