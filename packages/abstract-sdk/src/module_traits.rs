64e7f76c10fbf1557de461c54d215082521e1dec
CyberHoward <CyberHoward@protonmail.com>
2022-09-07 10:05:01 +0000

    update sdk

--- packages/abstract-sdk/src/module_traits.rs
+++ packages/abstract-sdk/src/module_traits.rs
@@ -10,9 +10,11 @@ pub trait OsExecute {
     fn os_execute(&self, deps: Deps, msgs: Vec<CosmosMsg>) -> Result<Response, Self::Err>;
 }
 
-// easily retrieve the memory object from the contract to perform queries
+/// easily retrieve the memory object from the contract to perform queries
 pub trait MemoryOperation {
+    /// Load the Memory object
     fn load_memory(&self, store: &dyn Storage) -> StdResult<Memory>;
+    /// Resolve a query on the memory contract
     fn resolve<T: Resolve>(&self, deps: Deps, memory_entry: &T) -> StdResult<T::Output> {
         memory_entry.resolve(deps, &self.load_memory(deps.storage)?)
     }
