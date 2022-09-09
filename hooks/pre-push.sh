514c23b33fdc7f71a3914a78d54c17d63fe7cf24
CyberHoward <CyberHoward@protonmail.com>
2022-09-09 17:26:45 +0000

    add schema gen to pre-push script

--- hooks/pre-push.sh
+++ hooks/pre-push.sh
@@ -37,49 +37,14 @@ fi
 format_check() {
   printf "Starting file formatting check...\n"
 
-  has_formatting_issues=0
-  first_file=1
-  rust_staged_files=$(git diff --name-only --staged -- '*.rs')
-
-  # check for issues
-  for file in $rust_staged_files; do
-    format_check_result=$(rustfmt --check $file)
-    if [ "$format_check_result" != "" ]; then
-      if [ $first_file -eq 0 ]; then
-        printf "\n"
-      fi
-      printf "$file"
-      has_formatting_issues=1
-      first_file=0
+  cd $project_toplevel || exit;
+  cargo fmt;
+  cargo workspaces exec --no-bail cargo schema;
+  not_staged_file=$(git diff --name-only)
+    if [ "$not_staged_file" != "" ]; then # it means the file changed and it's not staged, i.e. rustfmt did the job.
+      git add .
+      git commit -m "formatting and schema gen"
     fi
-  done
-
-  if [ $has_formatting_issues -ne 0 ]; then # there are formatting issues
-    printf "\nFormatting issues were found in files listed above. Trying to format them for you...\n"
-    exit_code=0
-
-    for file in $rust_staged_files; do
-      rustfmt $file
-      format_exit_code=$?
-
-      if [ $format_exit_code -ne 0 ]; then
-        # rustfmt couldn't format the current file
-        exit_code=1
-      else
-        not_staged_file=$(git diff --name-only -- $file)
-
-        if [ "$not_staged_file" != "" ]; then # it means the file changed and it's not staged, i.e. rustfmt did the job.
-          git add $not_staged_file
-        fi
-      fi
-    done
-
-    if [ $exit_code -ne 0 ]; then
-      printf "rustfmt failed to format some files. Please review, fix them and stage them manually."
-      exit 1
-    fi
-
-  fi
 }
 
 # clippy checks
@@ -94,5 +59,53 @@ lint_check() {
   fi
 }
 
+# # schema checks
+# schema_check() {
+#   printf "Starting file formatting check...\n"
+
+#   has_formatting_issues=0
+#   first_file=1
+#   rust_staged_files=$(git diff --name-only --staged -- '*.rs')
+
+#   # check for issues
+#   for file in $rust_staged_files; do
+#     format_check_result=$(rustfmt --check $file)
+#     if [ "$format_check_result" != "" ]; then
+#       if [ $first_file -eq 0 ]; then
+#         printf "\n"
+#       fi
+#       printf "$file"
+#       has_formatting_issues=1
+#       first_file=0
+#     fi
+#   done
+
+#   if [ $has_formatting_issues -ne 0 ]; then # there are formatting issues
+#     printf "\nFormatting issues were found in files listed above. Trying to format them for you...\n"
+#     exit_code=0
+
+#     for file in $rust_staged_files; do
+#       rustfmt $file
+#       format_exit_code=$?
+
+#       if [ $format_exit_code -ne 0 ]; then
+#         # rustfmt couldn't format the current file
+#         exit_code=1
+#       else
+#         not_staged_file=$(git diff --name-only -- $file)
+
+#         if [ "$not_staged_file" != "" ]; then # it means the file changed and it's not staged, i.e. rustfmt did the job.
+#           git add $not_staged_file
+#         fi
+#       fi
+#     done
+
+#     if [ $exit_code -ne 0 ]; then
+#       printf "rustfmt failed to format some files. Please review, fix them and stage them manually."
+#       exit 1
+#     fi
+
+#   fi
+# }
 format_check
 lint_check
