53bc6e32e599c4d429e697a974cab0b9d0bab8b2
adairrr <32375605+adairrr@users.noreply.github.com>
2022-12-16 12:20:10 +0100

    Add constant product pool

--- packages/abstract-os/src/objects/pool/pool_type.rs
+++ packages/abstract-os/src/objects/pool/pool_type.rs
@@ -7,19 +7,26 @@ use std::str::FromStr;
 #[derive(Deserialize, Serialize, Clone, Debug, PartialEq, Eq, JsonSchema)]
 #[non_exhaustive]
 pub enum PoolType {
+    ConstantProduct,
     Stable,
     Weighted,
     LiquidityBootstrap,
 }
+
+const CONSTANT_PRODUCT: &str = "constant_product";
+const STABLE: &str = "stable";
+const WEIGHTED: &str = "weighted";
+const LIQUIDITY_BOOTSTRAP: &str = "liquidity_bootstrap";
 
 impl FromStr for PoolType {
     type Err = StdError;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         match s {
-            "stable" => Ok(PoolType::Stable),
-            "weighted" => Ok(PoolType::Weighted),
-            "liquidity_bootstrap" => Ok(PoolType::LiquidityBootstrap),
+            CONSTANT_PRODUCT => Ok(PoolType::ConstantProduct),
+            STABLE => Ok(PoolType::Stable),
+            WEIGHTED => Ok(PoolType::Weighted),
+            LIQUIDITY_BOOTSTRAP => Ok(PoolType::LiquidityBootstrap),
             _ => Err(StdError::generic_err(format!("invalid pool type `{}`", s))),
         }
     }
@@ -36,9 +43,10 @@ impl TryFrom<String> for PoolType {
 impl fmt::Display for PoolType {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
-            PoolType::Stable => write!(f, "stable"),
-            PoolType::Weighted => write!(f, "weighted"),
-            PoolType::LiquidityBootstrap => write!(f, "liquidity_bootstrap"),
+            PoolType::ConstantProduct => write!(f, "{}", CONSTANT_PRODUCT),
+            PoolType::Stable => write!(f, "{}", STABLE),
+            PoolType::Weighted => write!(f, "{}", WEIGHTED),
+            PoolType::LiquidityBootstrap => write!(f, "{}", LIQUIDITY_BOOTSTRAP),
         }
     }
 }
