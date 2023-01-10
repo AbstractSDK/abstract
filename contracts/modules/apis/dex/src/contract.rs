f9b57613b69d48c81d172c6e907dacc048d416fa
cyberhoward <cyberhoward@protonmail.com>
2023-01-10 12:33:17 +0100

    re-enable library feature flag on osmosis host

--- contracts/modules/apis/dex/src/contract.rs
+++ contracts/modules/apis/dex/src/contract.rs
@@ -17,8 +17,6 @@ pub const DEX_API: DexApi = DexApi::new(EXCHANGE, CONTRACT_VERSION, None)
     .with_execute(handlers::execute_handler)
     .with_query(handlers::query_handler);
 
-// Fails in integration testing, hency why it's commented out
-// // don't export endpoints when imported as library
-// #[cfg(not(feature = "library"))]
-// Export the endpoints for this contract
+// don't export endpoints when imported as library
+#[cfg(not(feature = "library"))]
 export_endpoints!(DEX_API, DexApi);
