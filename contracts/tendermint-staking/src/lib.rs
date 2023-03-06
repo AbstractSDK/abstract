456b5f8c85af2db18495493c827af172b8067e76
cyberhoward <cyberhoward@protonmail.com>
2023-03-06 13:12:16 +0100

    implement deployer trait

--- contracts/tendermint-staking/src/lib.rs
+++ contracts/tendermint-staking/src/lib.rs
@@ -7,14 +7,18 @@ pub const TENDERMINT_STAKING: &str = "abstract:tendermint-staking";
 
 #[cfg(feature = "boot")]
 pub mod boot {
+    use abstract_boot::ApiDeployer;
     use abstract_os::api::InstantiateMsg;
     use boot_core::{prelude::boot_contract, BootEnvironment, Contract};
     use cosmwasm_std::Empty;
+    use cw_multi_test::ContractWrapper;
 
     use crate::msg::*;
 
     #[boot_contract(InstantiateMsg, ExecuteMsg, QueryMsg, Empty)]
     pub struct TMintStakingApi<Chain>;
+
+    impl<Chain: BootEnvironment> ApiDeployer<Chain, Empty> for TMintStakingApi<Chain> {}
 
     impl<Chain: BootEnvironment> TMintStakingApi<Chain> {
         pub fn new(name: &str, chain: Chain) -> Self {
@@ -22,9 +26,9 @@ pub mod boot {
                 Contract::new(name, chain)
                     .with_wasm_path("tendermint_staking")
                     .with_mock(Box::new(ContractWrapper::new_with_empty(
-                        ::contract::execute,
-                        ::contract::instantiate,
-                        ::contract::query,
+                        crat::contract::execute,
+                        crat::contract::instantiate,
+                        crat::contract::query,
                     ))),
             )
         }
