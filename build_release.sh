e1e60fdf8557904a6d62a0eeaeb6d72e2d3b0755
cyberhoward <cyberhoward@protonmail.com>
2023-03-30 20:21:55 +0200

    bump optimizer

--- build_release.sh
+++ build_release.sh
@@ -4,4 +4,4 @@
 docker run --rm -v "$(pwd)":/code \
   --mount type=volume,source="$(basename "$(pwd)")_cache",target=/code/target \
   --mount type=volume,source=registry_cache,target=/usr/local/cargo/registry \
-  cosmwasm/workspace-optimizer-arm64:0.12.12
+  cosmwasm/workspace-optimizer-arm64:0.12.13
