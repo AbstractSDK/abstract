5e75e535c34d626e3753526e813c0a3c36a12dac
Kayanski <kowalski.kowalskin@gmail.com>
2023-06-09 08:43:19 +0200

    Changed identity trait

--- packages/utils/src/identity.rs
+++ packages/utils/src/identity.rs
@@ -1,4 +1,7 @@
 pub trait Identify {
-    fn over_ibc(&self) -> bool;
+    /// This should return wether the platform is available on the chain designated by chain_name
+    /// For instance, Wyndex is available on juno-1, so wyndex.is_available_on("juno") should return true
+    /// We will only pass the chain name and never the chain_id to this function
+    fn is_available_on(&self, chain_name: &str) -> bool;
     fn name(&self) -> &'static str;
 }
