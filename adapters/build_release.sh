a9099c2983a17033f7ff8d1efc9bc67c0bd5feb9
cyberhoward <cyberhoward@protonmail.com>
2023-07-04 19:31:09 +0200

    fix adapter wasming

--- adapters/build_release.sh
+++ adapters/build_release.sh
@@ -4,13 +4,13 @@
 rm -rf ./artifacts/*.wasm
 
 if [[ $(arch) == "arm64" ]]; then
-  image="abstractmoney/workspace-optimizer-arm64"
+  image="cosmwasm/workspace-optimizer-arm64"
 else
-  image="abstractmoney/workspace-optimizer"
+  image="cosmwasm/workspace-optimizer"
 fi
 
 # Optimized builds
 docker run --rm -v "$(pwd)":/code \
   --mount type=volume,source="$(basename "$(pwd)")_cache",target=/code/target \
   --mount type=volume,source=registry_cache,target=/usr/local/cargo/registry \
-  ${image}:0.12.14
+  ${image}:0.12.13
---
+++ scripts/wasm-all.sh
@@ -0,0 +1,63 @@
+#!/usr/bin/env bash
+
+# Detect the architecture #
+if [[ $(arch) == "arm64" ]]; then
+image="cosmwasm/rust-optimizer-arm64"
+workspace_image="cosmwasm/workspace-optimizer-arm64"
+abstract_image="abstractsdk/workspace-optimizer-arm64"
+else
+image="cosmwasm/rust-optimizer"
+workspace_image="cosmwasm/workspace-optimizer"
+abstract_image="abstractsdk/workspace-optimizer"
+fi
+
+current_dir=$(pwd)
+
+# echo "Wasming app-template"
+# cd ./app-template
+
+# # Delete all the current wasms first
+# rm -rf ./artifacts/*.wasm
+# # Optimized builds
+# docker run --rm -v "$(pwd)":/code \
+# --mount type=volume,source="$(basename "$(pwd)")_cache",target=/code/target \
+# --mount type=volume,source=registry_cache,target=/usr/local/cargo/registry \
+# ${image}:0.12.13
+
+# cd $current_dir
+
+
+# echo "Wasming apps"
+# cd ./apps
+
+# # Delete all the current wasms first
+# rm -rf ./artifacts/*.wasm
+# # Optimized builds
+# docker run --rm -v "$(pwd)":/code \
+# -v "$(dirname "$(pwd)")/framework":/framework \
+# -v "$(dirname "$(pwd)")/adapters":/adapters \
+# -v "$(dirname "$(pwd)")/integrations":/integrations \
+# -v "$(dirname "$(pwd)")/integration-bundles":/integration-bundles \
+# --mount type=volume,source="$(basename "$(pwd)")_cache",target=/code/target \
+# --mount type=volume,source=registry_cache,target=/usr/local/cargo/registry \
+# ${workspace_image}:0.12.13
+
+# cd $current_dir
+
+echo "Wasming adapters"
+cd ./adapters
+
+# Delete all the current wasms first
+rm -rf ./artifacts/*.wasm
+# Optimized builds
+docker run --rm -v "$(pwd)":/code \
+-v "$(dirname "$(pwd)")/framework":/framework \
+-v "$(dirname "$(pwd)")/apps":/apps \
+-v "$(dirname "$(pwd)")/integrations":/integrations \
+-v "$(dirname "$(pwd)")/adapter-packages":/adapter-packages \
+-v "$(dirname "$(pwd)")/integration-bundles":/integration-bundles \
+--mount type=volume,source="$(basename "$(pwd)")_cache",target=/code/target \
+--mount type=volume,source=registry_cache,target=/usr/local/cargo/registry \
+${abstract_image}:0.12.14
+
+cd $current_dir
