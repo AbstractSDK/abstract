bd7459d090de57d81f2875f137e963fdab3756d8
cyberhoward <cyberhoward@protonmail.com>
2023-07-05 17:14:01 +0200

    comment out version tagging

--- framework/publish/packages.sh
+++ framework/publish/packages.sh
@@ -25,6 +25,6 @@ done
 
 echo "Everything is published!"
 
-VERSION=$(grep -A1 "\[workspace.package\]" Cargo.toml | awk -F'"' '/version/ {print $2}');
-git tag v"$VERSION"
-git push origin v"$VERSION"
+# VERSION=$(grep -A1 "\[workspace.package\]" Cargo.toml | awk -F'"' '/version/ {print $2}');
+# git tag v"$VERSION"
+# git push origin v"$VERSION"
