ef16d1f6b807b3bb96c1ffd97e337695df7e545a
adairrr <32375605+adairrr@users.noreply.github.com>
2023-08-16 11:09:59 +0200

    Remove gas station app

--- modules/contracts/apps/gas-station/src/handlers/mod.rs
+++
@@ -1,9 +0,0 @@
-pub mod execute;
-pub mod instantiate;
-pub mod query;
-pub mod sudo;
-
-pub use crate::handlers::{
-    execute::execute_handler, instantiate::instantiate_handler, query::query_handler,
-    sudo::sudo_handler,
-};
--- modules/contracts/apps/gas-station/src/replies/mod.rs
+++
@@ -1,5 +0,0 @@
-mod create_denom_reply;
-
-pub const CREATE_DENOM_REPLY_ID: u64 = 15;
-
-pub use create_denom_reply::create_denom_reply;
