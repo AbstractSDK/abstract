820eaeb5c4d439cd033debb7ab35d3b7cdc31f36
0xmovses <r.v.melkonian@gmail.com>
2023-08-23 12:39:26 +0100

    update handlers

--- modules/contracts/apps/challenge/src/handlers/execute.rs
+++ modules/contracts/apps/challenge/src/handlers/execute.rs
@@ -1,3 +1,4 @@
+use abstract_app::AppError;
 use abstract_core::objects::{AssetEntry, DexName};
 use abstract_dex_adapter::msg::OfferAsset;
 use abstract_sdk::features::AbstractResponse;
@@ -10,7 +11,9 @@ use crate::contract::{AppResult, ChallengeApp};
 
 use crate::error::AppError;
 use crate::msg::{AccExecuteMsg, ExecuteMsg, Frequency};
-use crate::state::{AccEntry, ChallengeEntry, Config, CHALLENGE_LIST, CONFIG, NEXT_ID};
+use crate::state::{
+    AccEntry, ChallengeEntry, Config, Friend, CHALLENGE_FRIENDS, CHALLENGE_LIST, CONFIG, NEXT_ID,
+};
 use abstract_dex_adapter::api::DexInterface;
 use abstract_sdk::AbstractSdkResult;
 use croncat_app::croncat_intergration_utils::{CronCatAction, CronCatTaskRequest};
@@ -40,7 +43,7 @@ fn update_config(
 }
 
 /// Create new Accountability
-fn create_accountability(
+fn create_challenge(
     deps: DepsMut,
     env: Env,
     info: MessageInfo,
@@ -62,7 +65,7 @@ fn create_accountability(
         source_asset,
         frequency,
     };
-    ACC_LIST.save(deps.storage, dca_id.clone(), &acc_entry)?;
+    CHALLENGE_LIST.save(deps.storage, dca_id.clone(), &acc_entry)?;
 
     let cron_cat = app.cron_cat(deps.as_ref());
     //let task_msg =
@@ -76,7 +79,7 @@ fn create_accountability(
 }
 
 /// Update an existing challenge  
-fn update_accountability(
+fn update_challenge(
     deps: DepsMut,
     env: Env,
     info: MessageInfo,
@@ -89,7 +92,7 @@ fn update_accountability(
 
     // Only if frequency is changed we have to re-create a task
     let recreate_task = new_frequency.is_some();
-    let old_accountability = ACC_LIST.load(deps.storage, acc_id.clone())?;
+    let old_accountability = CHALLENGE_LIST.load(deps.storage, acc_id.clone())?;
     let new_accountability = ChallengeEntry {
         name: new_name.unwrap_or(old_accountability.name),
         source_asset: new_source_asset.unwrap_or(old_accountability.source_asset),
@@ -106,7 +109,7 @@ fn update_accountability(
     };
 }
 
-fn cancel_accountability(
+fn cancel_challenge(
     deps: DepsMut,
     info: MessageInfo,
     app: ChallengeApp,
@@ -114,7 +117,7 @@ fn cancel_accountability(
 ) -> AppResult {
     app.admin.assert_admin(deps.as_ref(), &info.sender)?;
 
-    ACC_LIST.remove(deps.storage, acc_id.clone());
+    CHALLENGE_LIST.remove(deps.storage, acc_id.clone());
 
     let cron_cat = app.cron_cat(deps.as_ref());
     let remove_task_msg = cron_cat.remove_task(acc_id.clone())?;
@@ -125,21 +128,88 @@ fn cancel_accountability(
     ))
 }
 
-fn add_friend(
+fn add_friend_for_challenge(
     deps: DepsMut,
     info: MessageInfo,
-    app: AccApp,
+    app: &AccApp,
+    challenge_id: u64,
+    friend_name: String,
     friend_address: String,
-    balance: Uint128,
 ) -> AppResult {
     app.admin.assert_admin(deps.as_ref(), &info.sender)?;
+
+    if CHALLENGE_FRIENDS
+        .may_load(deps.storage, (&friend_address, challlenge_id))?
+        .is_some()
+    {
+        return AppError::generic_err("Friend already added for this challenge").into();
+    }
+
+    let friend = Friend {
+        address: friend_address.clone(),
+        name: friend_name,
+    };
+
+    CHALLENGE_FRIENDS.save(deps.storage, (&friend_address, challlenge_id), &friend)?;
+    Ok(Response::new())
 }
 
-fn create_task_internal(
-    env: Env,
-    dca: AccEntry,
-    acc_id: String,
-    cron_cat: CronCat<AccApp>,
-    config: Config,
-) -> AbstractSdkResult<CosmosMsg> {
+pub fn remove_friend_from_challenge(
+    deps: DepsMut,
+    info: MessageInfo,
+    app: &ChallengeApp,
+    challenge_id: u64,
+    friend_address: String,
+) -> AppResult {
+    // Ensure the caller is an admin
+    app.admin.assert_admin(deps.as_ref(), &info.sender)?;
+
+    // Ensure the friend exists for this challenge before removing
+    if CHALLENGE_FRIENDS
+        .may_load(deps.storage, (&friend_address, challenge_id))?
+        .is_none()
+    {
+        return Err(AppError::NotFound {
+            kind: "Friend".to_string(),
+        });
+    }
+
+    CHALLENGE_FRIENDS.remove(deps.storage, (&friend_address, challenge_id));
+    Ok(Response::new())
+}
+
+fn add_friends_for_challenge(
+    deps: DepsMut,
+    info: MessageInfo,
+    app: &ChallengeApp,
+    challenge_id: u64,
+    friends: Vec<Friend>,
+) -> AppResult {
+    // Ensure the caller is an admin
+    app.admin.assert_admin(deps.as_ref(), &info.sender)?;
+
+    // Ensure the friends don't already exist for this challenge before adding
+    for friend in friends.iter() {
+        if CHALLENGE_FRIENDS
+            .may_load(deps.storage, (&friend.address, challenge_id))?
+            .is_some()
+        {
+            return Err(AppError::generic_err(
+                "Friend already added for this challenge",
+            ));
+        }
+    }
+
+    // Add the friends
+    for friend in friends.iter() {
+        CHALLENGE_FRIENDS.save(deps.storage, (&friend.address, challenge_id), &friend)?;
+    }
+
+    Ok(Response::new())
+}
+
+fn daily_check_in(deps: DepsMut, _env: Env, info: MessageInfo, app: &ChallengeApp) -> AppResult {
+    app.admin.assert_admin(deps.as_ref(), &info.sender)?;
+
+    let today = env.block.time.seconds() / 86400;
 }
--- modules/contracts/apps/challenge/src/state.rs
+++ modules/contracts/apps/challenge/src/state.rs
@@ -1,6 +1,7 @@
 use abstract_core::objects::{AssetEntry, DexName};
 use abstract_dex_adapter::msg::OfferAsset;
 use cosmwasm_std::{Decimal, Uint128};
+use croncat_app::croncat_integration_utils::CronCatInterval;
 use cw_storage_plus::{Item, Map};
 
 use crate::msg::Frequency;
@@ -33,6 +34,7 @@ pub struct Vote {
 
 pub const CONFIG: Item<Config> = Item::new("config");
 pub const NEXT_ID: Item<u64> = Item::new("next_id");
-pub const CHALLENGE_LIST: Map<String, ChallengeEntry> = Map::new("acc_list");
-pub const FRIENDS: Map<String, Friend> = Map::new("friends");
+pub const CHALLENGE_LIST: Map<String, ChallengeEntry> = Map::new("challenge_list");
+pub const CHALLENGE_FRIENDS: Map<(String, u64), Friend> = Map::new("challenge_friends");
 pub const VOTES: Map<u64, Vec<Vote>> = Map::new("votes");
+pub const DAILY_CHECKINS: Map<String, CronCatInterval> = Map::new("daily_checkins");
