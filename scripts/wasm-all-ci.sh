71d0a5769c17c64af31da55c660de47208e830b1
cyberhoward <cyberhoward@protonmail.com>
2023-09-04 10:00:46 +0200

    remove use of `just` and wasm modules as well

--- scripts/wasm-all-ci.sh
+++ scripts/wasm-all-ci.sh
@@ -13,26 +13,43 @@ fi
 
 starting_dir=$(pwd)
 
+# see https://github.com/CosmWasm/cw-plus/blob/main/.circleci/config.yml
 echo "Wasming framework"
 cd ./framework
 
 # Delete the current artifacts folder.
 rm -rf ./artifacts
 
-# see https://github.com/CosmWasm/cw-plus/blob/main/.circleci/config.yml
-
 # create a dummy container which will hold a volume with config
 docker create -v /code --name with_code alpine /bin/true
 # copy a config file into this volume
 docker cp Cargo.toml with_code:/code
 docker cp Cargo.lock with_code:/code
-
 # copy code into this volume
 docker cp ./contracts with_code:/code
 docker cp ./packages with_code:/code
 docker cp ./scripts with_code:/code
-
 docker run --volumes-from with_code ${abstract_image}:0.14.0
 docker cp with_code:/code/artifacts ./artifacts
+
+cd $starting_dir
+
+echo "Wasming modules"
+cd ./modules
+
+# Delete the current artifacts folder.
+rm -rf ./artifacts
+
+# create a dummy container which will hold a volume with config
+docker create -v /code --name modules_with_code alpine /bin/true
+# copy a config file into this volume
+docker cp Cargo.toml modules_with_code:/code
+docker cp Cargo.lock modules_with_code:/code
+# copy code into this volume
+docker cp ./contracts modules_with_code:/code
+docker cp ./packages modules_with_code:/code
+docker cp ./scripts modules_with_code:/code
+docker run --volumes-from modules_with_code ${abstract_image}:0.14.0
+docker cp modules_with_code:/code/artifacts ./artifacts
 
 cd $starting_dir
