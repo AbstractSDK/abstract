e89869ce69454a681dee37309855971f46a5ea69
cyberhoward <cyberhoward@protonmail.com>
2023-09-05 11:05:10 +0200

    update to try out codecov

---
+++ scripts/modules-coverage.sh
@@ -0,0 +1,21 @@
+# create a dummy container which will hold a volume with config
+docker create -v /code -v /integrations -v /framework --name modules_with_code alpine /bin/true
+
+# copy directories to container.
+docker cp ./integrations modules_with_code:/
+docker cp ./framework modules_with_code:/
+
+# go into the directory we want to compile
+cd ./modules
+
+# Install cargo-llvm-cov for coverage generation
+cargo install cargo-llvm-cov
+
+# Create lock file if it does not exist
+if [ ! -f Cargo.lock ]; then
+  cargo generate-lockfile
+fi
+
+cargo llvm-cov --locked --all-features --lcov --output-path lcov.info
+
+# Copy the coverage report back out
