15de9e14e0a94fcd9644287aa98e9466ca841d56
cyberhoward <cyberhoward@protonmail.com>
2023-09-05 12:19:19 +0200

    add llvm-cov install

--- scripts/modules-coverage.sh
+++ scripts/modules-coverage.sh
@@ -1,15 +1,11 @@
-# create a dummy container which will hold a volume with config
-# docker create -v /code -v /integrations -v /framework --name modules_with_code alpine /bin/true
-
-# # copy directories to container.
-# docker cp ./integrations modules_with_code:/
-# docker cp ./framework modules_with_code:/
-
 # go into the directory we want to compile
 cd ./modules
 
 # Install cargo-llvm-cov for coverage generation
-# cargo install cargo-llvm-cov
+# Get host target
+host=$(rustc -Vv | grep host | sed 's/host: //')
+# Download binary and install to $HOME/.cargo/bin
+curl -LsSf https://github.com/taiki-e/cargo-llvm-cov/releases/latest/download/cargo-llvm-cov-$host.tar.gz | tar xzf - -C $HOME/.cargo/bin
 
 # Create lock file if it does not exist
 if [ ! -f Cargo.lock ]; then
