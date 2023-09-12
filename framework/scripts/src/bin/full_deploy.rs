f94b7a15d446f2dd9c19d46c5945f3b96e83f033
0xmovses <r.v.melkonian@gmail.com>
2023-09-12 17:31:35 +0100

    ping url, write json log

--- framework/scripts/src/bin/full_deploy.rs
+++ framework/scripts/src/bin/full_deploy.rs
@@ -1,7 +1,9 @@
+use std::{fs, net::TcpStream, path::Path};
+
 use abstract_core::objects::gov_type::GovernanceDetails;
 use abstract_interface::Abstract;
 
-use abstract_interface_scripts::assert_wallet_balance;
+use abstract_interface_scripts::{assert_wallet_balance, DeploymentStatus};
 use clap::Parser;
 use cw_orch::{
     deploy::Deploy,
@@ -21,12 +23,34 @@ fn full_deploy(networks: Vec<ChainInfo>) -> anyhow::Result<()> {
     let networks = rt.block_on(assert_wallet_balance(&networks));
 
     for network in networks {
+        let urls = network.grpc_urls.to_vec();
+        for url in urls {
+            rt.block_on(ping_grpc(&url))?;
+        }
+
         let chain = DaemonBuilder::default()
             .handle(rt.handle())
             .chain(network.clone())
             .build()?;
+
         let sender = chain.sender();
-        let deployment = Abstract::deploy_on(chain, sender.to_string())?;
+        let deployment = Abstract::deploy_on(chain.clone(), sender.to_string())?;
+
+        let mut deployment_status = DeploymentStatus {
+            chain_id: network.chain_id.to_string(),
+            success: false, // Default to false
+        };
+
+        match Abstract::deploy_on(chain, sender.to_string()) {
+            Ok(_) => {
+                deployment_status.success = true;
+                write_deployment(&deployment_status)?;
+            }
+            Err(e) => {
+                write_deployment(&deployment_status)?;
+                return Err(e.into());
+            }
+        }
 
         // Create the Abstract Account because it's needed for the fees for the dex module
         deployment
@@ -35,6 +59,32 @@ fn full_deploy(networks: Vec<ChainInfo>) -> anyhow::Result<()> {
                 monarch: sender.to_string(),
             })?;
     }
+    Ok(())
+}
+
+async fn ping_grpc(url_str: &str) -> anyhow::Result<()> {
+    let parsed_url = url::Url::parse(url_str)?;
+
+    let host = parsed_url
+        .host_str()
+        .ok_or_else(|| anyhow::anyhow!("No host in url"))?;
+
+    let port = parsed_url.port_or_known_default().ok_or_else(|| {
+        anyhow::anyhow!(
+            "No port in url, and no default for scheme {:?}",
+            parsed_url.scheme()
+        )
+    })?;
+    let socket_addr = format!("{}:{}", host, port);
+
+    let _ = TcpStream::connect(socket_addr);
+    Ok(())
+}
+
+fn write_deployment(status: &DeploymentStatus) -> anyhow::Result<()> {
+    let path = Path::new("scripts").join("deployments.json");
+    let status_str = serde_json::to_string_pretty(status)?;
+    fs::write(path, status_str)?;
     Ok(())
 }
 
