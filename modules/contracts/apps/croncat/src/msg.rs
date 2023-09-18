42d967c3b13c1194e92a03244c8c5b2843726d12
Buckram <buckram123@gmail.com>
2023-09-18 14:03:30 +0300

    Update croncat README

--- modules/contracts/apps/croncat/src/msg.rs
+++ modules/contracts/apps/croncat/src/msg.rs
@@ -17,20 +17,34 @@ pub struct AppInstantiateMsg {}
 #[cfg_attr(feature = "interface", derive(cw_orch::ExecuteFns))]
 #[cfg_attr(feature = "interface", impl_into(ExecuteMsg))]
 pub enum AppExecuteMsg {
+    /// Update config
+    /// currently this method is just placeholder
     UpdateConfig {},
+    /// Create new croncat task
     CreateTask {
+        /// Task request
         task: Box<CronCatTaskRequest>,
+        /// Custom task tag, which helps to use it after creation
         task_tag: String,
+        /// List of assets to be attached to the task
         assets: AssetListUnchecked,
     },
+    /// Remove task by tag and withdraw unused cw20 by this task
     RemoveTask {
+        /// Task tag of tag which should get removed
         task_tag: String,
     },
+    /// Refill a task
     RefillTask {
+        /// Task tag of tag which should get refilled
         task_tag: String,
+        /// List of assets to be added to the task balance
         assets: AssetListUnchecked,
     },
+    /// Clean state
+    /// Note: it will not check if task is active or not, use it mindfully
     Purge {
+        /// List of task tags that should get cleaned from the state
         task_tags: Vec<String>,
     },
 }
@@ -70,47 +84,58 @@ pub enum AppQueryMsg {
     /// Get task info
     #[returns(croncat_sdk_tasks::types::TaskResponse)]
     TaskInfo {
+        /// The addr of creator of tasks
         creator_addr: String,
+        /// The tag of the task
         task_tag: String,
     },
     /// Get task balance
     #[returns(croncat_sdk_manager::types::TaskBalanceResponse)]
     TaskBalance {
+        /// The addr of creator of tasks
         creator_addr: String,
+        /// The tag of the task
         task_tag: String,
     },
-    /// Get manager address of task
+    /// Get croncat manager address of task
     /// Used to verify sender
     #[returns(Addr)]
     ManagerAddr {
+        /// The addr of creator of tasks
         creator_addr: String,
+        /// The tag of the task
         task_tag: String,
     },
 }
 
 #[cosmwasm_schema::cw_serde]
 pub struct ConfigResponse {
+    /// Config of the croncat module
     pub config: Config,
 }
 
 #[cosmwasm_schema::cw_serde]
 pub enum ActiveTasksResponse {
-    Unchecked {
-        tasks: Vec<(Addr, String)>,
-    },
+    /// List of tasks in the module state, without checking if it's still active on croncat side
+    Unchecked { tasks: Vec<(Addr, String)> },
+    /// Lists of tasks in the module state, with checking if it's still active on croncat side
     Checked {
+        /// tasks that are currently active
         scheduled_tasks: Vec<(Addr, String)>,
+        /// tasks that got removed and can safely get purged
         removed_tasks: Vec<(Addr, String)>,
     },
 }
 
 #[cosmwasm_schema::cw_serde]
 pub enum ActiveTasksByCreatorResponse {
-    Unchecked {
-        tasks: Vec<String>,
-    },
+    /// List of tasks in the module state, without checking if it's still active on croncat side
+    Unchecked { tasks: Vec<String> },
+    /// Lists of tasks in the module state, with checking if it's still active on croncat side
     Checked {
+        /// tasks that are currently active
         scheduled_tasks: Vec<String>,
+        /// tasks that got removed and can safely get purged
         removed_tasks: Vec<String>,
     },
 }
