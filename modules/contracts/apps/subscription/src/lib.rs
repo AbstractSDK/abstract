3e206fb4a5596c62c9c3ea3c13a1bd9905b2d468
Buckram <buckram123@gmail.com>
2023-09-26 18:11:04 +0300

    subscription migrate

---
+++ modules/contracts/apps/subscription/src/lib.rs
@@ -0,0 +1,77 @@
+mod commands;
+pub mod contract;
+pub mod error;
+pub mod msg;
+pub mod queries;
+pub mod state;
+
+pub const SUBSCRIPTION: &str = "abstract:subscription";
+
+#[cfg(feature = "interface")]
+pub mod interface {
+    use crate::msg::*;
+    use abstract_core::app::{BaseInstantiateMsg, InstantiateMsg as AppInitMsg};
+    use abstract_interface::AppDeployer;
+    use cosmwasm_std::{Decimal, Uint128};
+    use cw_asset::AssetInfoUnchecked;
+    use cw_orch::{interface, prelude::*};
+    use std::str::FromStr;
+
+    #[interface(InstantiateMsg, ExecuteMsg, QueryMsg, MigrateMsg)]
+    pub struct Subscription;
+
+    impl<Chain: CwEnv> AppDeployer<Chain> for Subscription<Chain> {}
+
+    impl<Chain: CwEnv> Uploadable for Subscription<Chain> {
+        fn wrapper(&self) -> <Mock as TxHandler>::ContractSource {
+            Box::new(ContractWrapper::new_with_empty(
+                crate::contract::execute,
+                crate::contract::instantiate,
+                crate::contract::query,
+            ))
+        }
+        fn wasm(&self) -> WasmPath {
+            artifacts_dir_from_workspace!()
+                .find_wasm_path("abstract_subscription")
+                .unwrap()
+        }
+    }
+
+    impl<Chain: CwEnv> Subscription<Chain> {
+        pub fn init_msg(
+            payment_denom: String,
+            token_addr: String,
+            ans_host_address: String,
+            factory_addr: String,
+            version_control_addr: String,
+        ) -> AppInitMsg<InstantiateMsg> {
+            AppInitMsg::<InstantiateMsg> {
+                base: BaseInstantiateMsg {
+                    ans_host_address,
+                    version_control_address: version_control_addr,
+                },
+                module: InstantiateMsg {
+                    subscription: SubscriptionInstantiateMsg {
+                        factory_addr,
+                        payment_asset: AssetInfoUnchecked::native(payment_denom),
+                        subscription_cost_per_block: Decimal::from_str("0.000001").unwrap(),
+                        subscription_per_block_emissions:
+                            crate::state::UncheckedEmissionType::IncomeBased(
+                                AssetInfoUnchecked::cw20(token_addr.clone()),
+                            ),
+                    },
+                    contribution: Some(ContributionInstantiateMsg {
+                        protocol_income_share: Decimal::percent(10),
+                        emission_user_share: Decimal::percent(50),
+                        max_emissions_multiple: Decimal::from_ratio(2u128, 1u128),
+                        token_info: AssetInfoUnchecked::cw20(token_addr),
+                        emissions_amp_factor: Uint128::new(680000),
+                        emissions_offset: Uint128::new(5200),
+                        // 3 days
+                        income_averaging_period: 259200u64.into(),
+                    }),
+                },
+            }
+        }
+    }
+}
