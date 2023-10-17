519aa4b2a4d281b6a33a5704544f733fede1bb17
Kayanski <kowalski.kowalskin@gmail.com>
2023-10-17 17:16:30 +0200

    Simple test for cw-orch full deploy

--- framework/scripts/src/bin/full_deploy.rs
+++ framework/scripts/src/bin/full_deploy.rs
@@ -1,16 +1,10 @@
-use reqwest::Url;
-use std::{
-    fs::{self, File},
-    io::BufReader,
-    net::TcpStream,
-};
-
 use abstract_core::objects::gov_type::GovernanceDetails;
 use abstract_interface::Abstract;
 
-use abstract_interface_scripts::{assert_wallet_balance, DeploymentStatus, SUPPORTED_CHAINS};
+use abstract_interface_scripts::SUPPORTED_CHAINS;
 use clap::Parser;
 use cw_orch::{
+    daemon::DaemonError,
     deploy::Deploy,
     prelude::{
         networks::{parse_network, ChainInfo},
@@ -29,101 +23,37 @@ fn full_deploy(mut networks: Vec<ChainInfo>) -> anyhow::Result<()> {
         networks = SUPPORTED_CHAINS.to_vec();
     }
 
-    let deployment_status = read_deployment()?;
-    if deployment_status.success {
-        log::info!("Do you want to re-deploy to {:?}?", networks);
-        let mut input = String::new();
-        std::io::stdin().read_line(&mut input)?;
-        if input.to_lowercase().contains('n') {
-            return Ok(());
-        }
-    }
-    // let deployment_status = deployment_status.clone();
+    // We fill the variable with what we need
 
-    // If some chains need to be deployed, deploy them
-    // if !deployment_status.chain_ids.is_empty() {
-    //     networks = deployment_status.chain_ids.into_iter().map(|n| parse_network(&n)).collect();
-    // }
+    let networks = networks
+        .iter()
+        .map(|n| {
+            // let chain = DaemonBuilder::default()
+            //     .handle(rt.handle())
+            //     .chain(n.clone())
+            //     .build()?;
 
-    let networks = rt.block_on(assert_wallet_balance(&networks));
+            let chain = Mock::with_chain_id(&Addr::unchecked("sender"), n.chain_id);
+            Ok::<_, DaemonError>((chain.clone(), chain.sender().to_string()))
+        })
+        .collect::<Result<Vec<_>, _>>()?;
 
-    // write_deployment(&deployment_status)?;
+    let _deployments = Abstract::full_deploy(
+        networks,
+        None,
+        Some(|abstr| {
+            abstr
+                .account_factory
+                .create_default_account(GovernanceDetails::Monarchy {
+                    monarch: abstr.account_factory.get_chain().sender().to_string(),
+                })?;
 
-    for network in networks {
-        let urls = network.grpc_urls.to_vec();
-        for url in urls {
-            rt.block_on(ping_grpc(url))?;
-        }
+            // anyhow::bail!("Artificial error for testing");
+            Ok(())
+        }),
+    )?;
 
-        let chain = DaemonBuilder::default()
-            .handle(rt.handle())
-            .chain(network.clone())
-            .build()?;
-
-        let sender = chain.sender();
-
-        let deployment = match Abstract::deploy_on(chain, sender.to_string()) {
-            Ok(deployment) => {
-                // write_deployment(&deployment_status)?;
-                deployment
-            }
-            Err(e) => {
-                // write_deployment(&deployment_status)?;
-                return Err(e.into());
-            }
-        };
-
-        // Create the Abstract Account because it's needed for the fees for the dex module
-        deployment
-            .account_factory
-            .create_default_account(GovernanceDetails::Monarchy {
-                monarch: sender.to_string(),
-            })?;
-    }
-
-    // fs::copy(Path::new("~/.cw-orchestrator/state.json"), to)
     Ok(())
-}
-
-async fn ping_grpc(url_str: &str) -> anyhow::Result<()> {
-    let parsed_url = Url::parse(url_str)?;
-
-    let host = parsed_url
-        .host_str()
-        .ok_or_else(|| anyhow::anyhow!("No host in url"))?;
-
-    let port = parsed_url.port_or_known_default().ok_or_else(|| {
-        anyhow::anyhow!(
-            "No port in url, and no default for scheme {:?}",
-            parsed_url.scheme()
-        )
-    })?;
-    let socket_addr = format!("{}:{}", host, port);
-
-    let _ = TcpStream::connect(socket_addr);
-    Ok(())
-}
-#[allow(dead_code)]
-fn write_deployment(status: &DeploymentStatus) -> anyhow::Result<()> {
-    let path = dirs::home_dir()
-        .unwrap()
-        .join(".cw-orchestrator")
-        .join("chains.json");
-    let status_str = serde_json::to_string_pretty(status)?;
-    fs::write(path, status_str)?;
-    Ok(())
-}
-
-fn read_deployment() -> anyhow::Result<DeploymentStatus> {
-    let path = dirs::home_dir()
-        .unwrap()
-        .join(".cw-orchestrator")
-        .join("chains.json");
-    let file = File::open(path)?;
-    let reader = BufReader::new(file);
-
-    // Read the JSON contents of the file as an instance of `DeploymentStatus`. If not present use default.
-    Ok(serde_json::from_reader(reader).unwrap_or_default())
 }
 
 #[derive(Parser, Default, Debug)]
