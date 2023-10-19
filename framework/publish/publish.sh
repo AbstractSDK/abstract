9ce97e7f25fb568cead77e2e77007c3ae308b399
cyberhoward <cyberhoward@protonmail.com>
2023-10-19 10:52:54 +0200

    update publish

--- framework/publish/publish.sh
+++ framework/publish/publish.sh
@@ -76,10 +76,19 @@ echo "All the contracts are published!"
 # Now all the packages and standards
 
 PACKAGES="abstract-interface abstract-adapter abstract-app abstract-ibc-host"
+STANDARDS="utils staking dex"
+
+for pack in $STANDARDS; do
+  (
+    cd "packages/$pack"
+    echo "Publishing $pack"
+    publish_crate
+  )
+done
 
 for pack in $PACKAGES; do
   (
-    cd "packages/$pack"
+    cd "packages/standards/$pack"
     echo "Publishing $pack"
     publish_crate
   )
