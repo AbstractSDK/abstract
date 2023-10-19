330002d8571b9ba7def25ae3124399faee1bf3fe
CyberHoward <88450409+CyberHoward@users.noreply.github.com>
2023-10-19 19:41:59 +0200

    Fix challange app (#96)
    * replace duration enum
    
    * formatting
    
    * small refactor
    
    * remove check_ins
    
    * voting object
    
    * update voting
    
    * add vote error
    
    * fix challenge status
    
    * challenge execute methods
    
    * add expiration for voting
    
    * veto period start after votes counted
    
    * integrate voting in challenge
    
    * fix remove voters
    
    * add veto action
    
    * update queries
    
    * add comments
    
    * fix existing tests
    
    * add challenge id to the response
    
    * format
    
    * previous votes
    
    * wasm can't deserialize map
    
    * fix friends
    
    * bugfix
    
    * rename to proposals, where it fits
    
    * add votes query
    
    * cast vote as abstract account
    
    * add veto tests
    
    * format
    
    * add limits
    
    * Doc-comment for SimpleVoting
    
    * clippy
    
    * Bump challenge to 0.2.0
    
    * add validation, make description optional
    
    * format
    
    * duplicate addrs fix
    
    Thanks @adairrr for noticing this bug
    
    * update proposal initiation and veto
    
    * update tests/fix bugs
    
    * clippy fixes
    
    * add readme for the challenge
    
    * small edit in readme
    
    * voting tests and fixes
    
    * update challenge to the new simple_voting
    
    * add active_proposal to ChallengeEntryResponse
    
    * update schema
    
    * update voting readme
    
    * redundant take(1)
    
    * add proposal lifecylce
    
    * formatting
    
    * update proposal lifecylce
    
    * complete proposal lifecycle
    
    * fix beta clippy
    
    ---------
    
    Co-authored-by: Buckram <buckram123@gmail.com>
    Co-authored-by: adairrr <32375605+adairrr@users.noreply.github.com>

--- modules/contracts/apps/challenge/src/handlers/query.rs
+++ modules/contracts/apps/challenge/src/handlers/query.rs
@@ -1,43 +1,66 @@
 use crate::contract::{AppResult, ChallengeApp};
 use crate::msg::{
-    ChallengeQueryMsg, ChallengeResponse, ChallengesResponse, CheckInsResponse, FriendsResponse,
-    VoteResponse,
+    ChallengeEntryResponse, ChallengeQueryMsg, ChallengeResponse, ChallengesResponse,
+    FriendsResponse, ProposalsResponse, VoteResponse, VotesResponse,
 };
-use crate::state::{
-    ChallengeEntry, Vote, CHALLENGE_FRIENDS, CHALLENGE_LIST, DAILY_CHECK_INS, VOTES,
-};
-use cosmwasm_std::{to_binary, Binary, Deps, Env, Order, StdResult};
+use crate::state::{CHALLENGES, CHALLENGE_FRIENDS, CHALLENGE_PROPOSALS, SIMPLE_VOTING};
+use abstract_core::objects::voting::{ProposalId, ProposalInfo, VoteResult, DEFAULT_LIMIT};
+use cosmwasm_std::{to_binary, Binary, BlockInfo, Deps, Env, Order, StdResult};
 use cw_storage_plus::Bound;
+
+use super::execute::last_proposal;
 
 pub fn query_handler(
     deps: Deps,
-    _env: Env,
+    env: Env,
     app: &ChallengeApp,
     msg: ChallengeQueryMsg,
 ) -> AppResult<Binary> {
     match msg {
         ChallengeQueryMsg::Challenge { challenge_id } => {
-            to_binary(&query_challenge(deps, app, challenge_id)?)
+            to_binary(&query_challenge(deps, env, app, challenge_id)?)
         }
         ChallengeQueryMsg::Challenges { start_after, limit } => {
-            to_binary(&query_challenges(deps, start_after, limit)?)
+            to_binary(&query_challenges(deps, env, start_after, limit)?)
         }
         ChallengeQueryMsg::Friends { challenge_id } => {
             to_binary(&query_friends(deps, app, challenge_id)?)
         }
-        ChallengeQueryMsg::CheckIns { challenge_id } => {
-            to_binary(&query_check_in(deps, app, challenge_id)?)
-        }
         ChallengeQueryMsg::Vote {
-            last_check_in,
             voter_addr,
             challenge_id,
-        } => to_binary(&query_vote_for_check_in(
+            proposal_id,
+        } => to_binary(&query_vote(
             deps,
             app,
             voter_addr,
-            last_check_in,
             challenge_id,
+            proposal_id,
+        )?),
+        ChallengeQueryMsg::Proposals {
+            challenge_id,
+            start_after,
+            limit,
+        } => to_binary(&query_proposals(
+            deps,
+            env,
+            app,
+            challenge_id,
+            start_after,
+            limit,
+        )?),
+        ChallengeQueryMsg::Votes {
+            challenge_id,
+            proposal_id,
+            start_after,
+            limit,
+        } => to_binary(&query_votes(
+            deps,
+            app,
+            challenge_id,
+            proposal_id,
+            start_after,
+            limit,
         )?),
     }
     .map_err(Into::into)
@@ -45,53 +68,142 @@ pub fn query_handler(
 
 fn query_challenge(
     deps: Deps,
+    env: Env,
     _app: &ChallengeApp,
     challenge_id: u64,
 ) -> AppResult<ChallengeResponse> {
-    let challenge = CHALLENGE_LIST.may_load(deps.storage, challenge_id)?;
+    let challenge = CHALLENGES.may_load(deps.storage, challenge_id)?;
+
+    let proposal = get_proposal_if_active(challenge_id, deps, &env.block)?;
+    let challenge =
+        challenge.map(|entry| ChallengeEntryResponse::from_entry(entry, challenge_id, proposal));
     Ok(ChallengeResponse { challenge })
 }
 
-fn query_challenges(deps: Deps, start: u64, limit: u32) -> AppResult<ChallengesResponse> {
-    let challenges: StdResult<Vec<ChallengeEntry>> = CHALLENGE_LIST
-        .range(
-            deps.storage,
-            Some(Bound::exclusive(start)),
-            Some(Bound::inclusive(limit)),
-            Order::Ascending,
-        )
-        .map(|result| result.map(|(_, entry)| entry)) // strip the keys
-        .collect::<StdResult<Vec<ChallengeEntry>>>();
-    Ok(ChallengesResponse(challenges.unwrap_or_default()))
+fn get_proposal_if_active(
+    challenge_id: u64,
+    deps: Deps,
+    block: &BlockInfo,
+) -> Result<Option<ProposalInfo>, crate::error::AppError> {
+    let maybe_id = last_proposal(challenge_id, deps)?;
+    let proposal = maybe_id
+        .map(|id| {
+            let proposal = SIMPLE_VOTING.load_proposal(deps.storage, block, id)?;
+            if proposal.assert_active_proposal().is_ok() {
+                AppResult::Ok(Some(proposal))
+            } else {
+                AppResult::Ok(None)
+            }
+        })
+        .transpose()?
+        .flatten();
+    Ok(proposal)
+}
+
+fn query_challenges(
+    deps: Deps,
+    env: Env,
+    start: Option<u64>,
+    limit: Option<u64>,
+) -> AppResult<ChallengesResponse> {
+    let min = start.map(Bound::exclusive);
+    let limit = limit.unwrap_or(DEFAULT_LIMIT);
+
+    let challenges = CHALLENGES
+        .range(deps.storage, min, None, Order::Ascending)
+        .take(limit as usize)
+        .map(|result| {
+            result
+                .map_err(Into::into)
+                // Cast result into response
+                .map(|(challenge_id, entry)| {
+                    let proposal =
+                        get_proposal_if_active(challenge_id, deps, &env.block).unwrap_or_default();
+                    ChallengeEntryResponse::from_entry(entry, challenge_id, proposal)
+                })
+        })
+        .collect::<AppResult<Vec<ChallengeEntryResponse>>>()?;
+    Ok(ChallengesResponse { challenges })
 }
 
 fn query_friends(deps: Deps, _app: &ChallengeApp, challenge_id: u64) -> AppResult<FriendsResponse> {
     let friends = CHALLENGE_FRIENDS.may_load(deps.storage, challenge_id)?;
-    Ok(FriendsResponse(friends.unwrap_or_default()))
+    Ok(FriendsResponse {
+        friends: friends.unwrap_or_default(),
+    })
 }
 
-fn query_check_in(
-    deps: Deps,
-    _app: &ChallengeApp,
-    challenge_id: u64,
-) -> AppResult<CheckInsResponse> {
-    let check_ins = DAILY_CHECK_INS.may_load(deps.storage, challenge_id)?;
-    Ok(CheckInsResponse(check_ins.unwrap_or_default()))
-}
-
-fn query_vote_for_check_in(
+fn query_vote(
     deps: Deps,
     _app: &ChallengeApp,
     voter_addr: String,
-    last_check_in: u64,
     challenge_id: u64,
+    proposal_id: Option<u64>,
 ) -> AppResult<VoteResponse> {
-    let v = Vote {
-        voter: voter_addr,
-        approval: None,
-        for_check_in: None,
+    let voter = deps.api.addr_validate(&voter_addr)?;
+    let maybe_proposal_id = if let Some(proposal_id) = proposal_id {
+        // Only allow loading proposal_id for this challenge
+        CHALLENGE_PROPOSALS
+            .may_load(deps.storage, (challenge_id, proposal_id))?
+            .map(|_| proposal_id)
+    } else {
+        last_proposal(challenge_id, deps)?
     };
-    let v = v.check(deps)?;
-    let vote = VOTES.may_load(deps.storage, (challenge_id, last_check_in, v.voter))?;
+    let vote = if let Some(proposal_id) = maybe_proposal_id {
+        SIMPLE_VOTING.load_vote(deps.storage, proposal_id, &voter)?
+    } else {
+        None
+    };
     Ok(VoteResponse { vote })
+}
+
+fn query_proposals(
+    deps: Deps,
+    env: Env,
+    _app: &ChallengeApp,
+    challenge_id: u64,
+    start_after: Option<ProposalId>,
+    limit: Option<u64>,
+) -> AppResult<ProposalsResponse> {
+    let min = start_after.map(Bound::exclusive);
+    let limit = limit.unwrap_or(DEFAULT_LIMIT);
+    let ids: Vec<ProposalId> = CHALLENGE_PROPOSALS
+        .prefix(challenge_id)
+        .keys(deps.storage, min, None, Order::Ascending)
+        .take(limit as usize)
+        .collect::<StdResult<_>>()?;
+    let results = ids
+        .into_iter()
+        .map(|id| {
+            SIMPLE_VOTING
+                .load_proposal(deps.storage, &env.block, id)
+                .map(|v| (id, v))
+        })
+        .collect::<VoteResult<Vec<(ProposalId, ProposalInfo)>>>()?;
+
+    Ok(ProposalsResponse { proposals: results })
+}
+
+fn query_votes(
+    deps: Deps,
+    _app: &ChallengeApp,
+    challenge_id: u64,
+    proposal_id: Option<u64>,
+    start_after: Option<cosmwasm_std::Addr>,
+    limit: Option<u64>,
+) -> AppResult<VotesResponse> {
+    let maybe_proposal_id = if let Some(proposal_id) = proposal_id {
+        // Only allow loading proposal_id for this challenge
+        CHALLENGE_PROPOSALS
+            .may_load(deps.storage, (challenge_id, proposal_id))?
+            .map(|_| proposal_id)
+    } else {
+        last_proposal(challenge_id, deps)?
+    };
+    let votes = if let Some(proposal_id) = maybe_proposal_id {
+        SIMPLE_VOTING.query_by_id(deps.storage, proposal_id, start_after.as_ref(), limit)?
+    } else {
+        vec![]
+    };
+    Ok(VotesResponse { votes })
 }
