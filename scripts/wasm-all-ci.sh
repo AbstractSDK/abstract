0f5311895cc37dc9a6c058a7301b4fd53c23f65a
cyberhoward <cyberhoward@protonmail.com>
2023-10-23 16:30:35 +0200

    update ci wasm script

--- scripts/wasm-all-ci.sh
+++ scripts/wasm-all-ci.sh
@@ -36,7 +36,7 @@ docker cp ./packages with_code:/code
 # Run the build
 docker run --volumes-from with_code ${abstract_image}:0.15.0
 # Copy the artifacts back out
-docker cp with_code:/code/artifacts .
+docker cp with_code:/code/artifacts/ .
 ls artifacts
 
 cd $starting_dir
@@ -67,10 +67,11 @@ docker cp Cargo.lock modules_with_code:/code
 # copy code into this volume
 docker cp ./contracts modules_with_code:/code
 docker cp ./packages modules_with_code:/code
+docker cp ./scripts modules_with_code:/code
 
 # Run the build
 docker run --volumes-from modules_with_code ${abstract_image}:0.15.0
 # Copy the artifacts back out
-docker cp modules_with_code:/code/artifacts .
+docker cp modules_with_code:/code/artifacts/ .
 ls artifacts
 cd $starting_dir
