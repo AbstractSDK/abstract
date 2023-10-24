e0e5f10fcb51be6cd4d18188d787562572d188b6
CyberHoward <88450409+CyberHoward@users.noreply.github.com>
2023-10-24 17:30:27 +0200

    Centralize scripts (#108)
    * move integrations around
    
    * fix repository links
    
    * fix dependencies and tests
    
    * fmt
    
    * format tomls
    
    * formatting
    
    * move module scripts and clean up tomls
    
    * add check for unused deps in CI
    
    * remove unused deps in framework
    
    * remove unused deps
    
    * remove unused schemas
    
    * formatting
    
    * remove wasm build from nightly build
    
    * update readme to contain monorepo structure.
    
    * formatting
    
    * add sccache to framework ci
    
    * rm duplicate import
    
    * install cargo workspace with stable rustup

--- integrations/wyndex-adapter/src/staking.rs
+++ integrations/wyndex-adapter/src/staking.rs
@@ -44,11 +44,11 @@ use {
     cw_asset::{AssetInfo, AssetInfoBase},
     cw_utils::Duration,
     wyndex_stake::msg::DistributionDataResponse,
+    wyndex_stake::msg::ReceiveDelegationMsg,
     wyndex_stake::{
         msg::{BondingInfoResponse, ExecuteMsg as StakeCw20ExecuteMsg},
         state::{BondingInfo, STAKE},
     },
-    wyndex_stake::msg::ReceiveDelegationMsg,
 };
 
 #[cfg(feature = "full_integration")]
