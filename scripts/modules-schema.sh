d49236116592b3b038f245f732d306957fd321e9
Buckram <buckram123@gmail.com>
2023-11-07 19:16:26 +0200

    path typo

--- scripts/modules-schema.sh
+++ scripts/modules-schema.sh
@@ -7,4 +7,4 @@ SCHEMA_OUT_DIR=$(cd ../schemas && echo "$PWD")
 cargo ws exec --no-bail bash -lc \
 'cargo schema && \
 { tmp=$(mktemp); jq ".contract_version = \"$(eval $version)\"" schema/module-schema.json > "$tmp" && mv "$tmp" schema/module-schema.json; \
-rm -rf "schema/raw"; outdir="$SCHEMA_OUT_DIR/abstract/${PWD##*}/$(eval $version)"; mkdir -p "$outdir"; cp -a "schema/." "$outdir";}'
+rm -rf "schema/raw"; outdir="$SCHEMA_OUT_DIR/abstract/${PWD##*/}/$(eval $version)"; mkdir -p "$outdir"; cp -a "schema/." "$outdir";}'
