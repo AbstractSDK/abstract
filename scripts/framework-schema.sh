57b200270042aec67e2f6359375a7e17536969ed
Buckram <buckram123@gmail.com>
2023-11-07 19:06:38 +0200

    typo fix

--- scripts/framework-schema.sh
+++ scripts/framework-schema.sh
@@ -5,4 +5,4 @@ version="cargo pkgid | cut -d@ -f2"
 SCHEMA_OUT_DIR=$(cd ../schemas && echo "$PWD")
 
 cargo ws exec --no-bail bash -lc \
-'cargo schema && { rm -rf "schema/raw"; outdir="$SCHEMA_OUT_DIR/abstract/${PWD##*}/$(eval $version); mkdir -p "$outdir"; cp -a "schema/." "$outdir";}'
+'cargo schema && { rm -rf "schema/raw"; outdir="$SCHEMA_OUT_DIR/abstract/${PWD##*}/$(eval $version)"; mkdir -p "$outdir"; cp -a "schema/." "$outdir";}'
