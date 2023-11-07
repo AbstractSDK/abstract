7a02b43a1512ef66e7b1948199f7db47fc46e42f
Buckram <buckram123@gmail.com>
2023-11-07 19:02:55 +0200

    copy schemas to ../../schema

--- scripts/framework-schema.sh
+++ scripts/framework-schema.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 cd ./framework
 
+version="cargo pkgid | cut -d@ -f2"
+SCHEMA_OUT_DIR=$(cd ../schemas && echo "$PWD")
+
 cargo ws exec --no-bail bash -lc \
-'cargo schema && rm -rf "schema/raw"'
+'cargo schema && { rm -rf "schema/raw"; outdir="$SCHEMA_OUT_DIR/abstract/${PWD##*}/$(eval $version); mkdir -p "$outdir"; cp -a "schema/." "$outdir";}'
--- scripts/modules-schema.sh
+++ scripts/modules-schema.sh
@@ -2,8 +2,9 @@
 cd ./modules
 
 version="cargo pkgid | cut -d@ -f2"
+SCHEMA_OUT_DIR=$(cd ../schemas && echo "$PWD")
 
 cargo ws exec --no-bail bash -lc \
 'cargo schema && \
 { tmp=$(mktemp); jq ".contract_version = \"$(eval $version)\"" schema/module-schema.json > "$tmp" && mv "$tmp" schema/module-schema.json; \
-rm -rf "schema/raw";}'
+rm -rf "schema/raw"; outdir="$SCHEMA_OUT_DIR/abstract/${PWD##*}/$(eval $version); mkdir -p "$outdir"; cp -a "schema/." "$outdir";}'
