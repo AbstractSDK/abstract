df6552710b19c93655084c3c08a246753f605c25
cyberhoward <cyberhoward@protonmail.com>
2023-11-08 15:05:01 +0100

    format

--- modules/contracts/adapters/tendermint-staking/src/contract.rs
+++ modules/contracts/adapters/tendermint-staking/src/contract.rs
@@ -14,8 +14,7 @@ pub type TendermintStakeAdapter = AdapterContract<
 >;
 
 const STAKING_ADAPTER: TendermintStakeAdapter =
-    TendermintStakeAdapter::new(TENDERMINT_STAKING, CONTRACT_VERSION, None)
-        .execute(handle_request);
+    TendermintStakeAdapter::new(TENDERMINT_STAKING, CONTRACT_VERSION, None).execute(handle_request);
 
 pub type TendermintStakeResult = Result<Response, TendermintStakeError>;
 
