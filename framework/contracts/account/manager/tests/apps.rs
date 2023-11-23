6b228f9d34b63ad1ee3f60f0165332e05d9c27fb
Buckram <buckram123@gmail.com>
2023-11-23 14:57:48 +0200

    tests

--- framework/contracts/account/manager/tests/apps.rs
+++ framework/contracts/account/manager/tests/apps.rs
@@ -1,11 +1,16 @@
 mod common;
-use abstract_app::gen_app_mock;
+use abstract_app::mock::MockError;
+use abstract_app::{gen_app_mock, mock};
+use abstract_core::manager::ModuleInstallConfig;
 use abstract_core::objects::account::TEST_ACCOUNT_ID;
+use abstract_core::objects::module::ModuleInfo;
+use abstract_core::objects::AccountId;
 use abstract_core::PROXY;
 use abstract_interface::*;
 
 use common::{create_default_account, AResult, TEST_COIN};
-use cosmwasm_std::{coin, Addr, Coin, CosmosMsg};
+use cosmwasm_std::{coin, to_json_binary, Addr, Coin, CosmosMsg};
+use cw_controllers::{AdminError, AdminResponse};
 use cw_orch::deploy::Deploy;
 use cw_orch::prelude::*;
 use speculoos::prelude::*;
@@ -80,5 +85,88 @@ fn account_install_app() -> AResult {
     let module_addr = account.manager.module_info(APP_ID)?.unwrap().address;
     assert_that!(app_addr).is_equal_to(module_addr);
     take_storage_snapshot!(chain, "account_install_app");
+    Ok(())
+}
+
+#[test]
+fn account_app_ownership() -> AResult {
+    let sender = Addr::unchecked(common::OWNER);
+    let chain = Mock::new(&sender);
+    let deployment = Abstract::deploy_on(chain.clone(), sender.to_string())?;
+    let account = create_default_account(&deployment.account_factory)?;
+
+    deployment
+        .version_control
+        .claim_namespace(TEST_ACCOUNT_ID, "tester".to_owned())?;
+
+    let app = MockApp::new_test(chain.clone());
+    app.deploy(APP_VERSION.parse().unwrap(), DeployStrategy::Try)?;
+    account.install_app(&app, &MockInitMsg, None)?;
+
+    let admin_res: AdminResponse =
+        app.query(&mock::QueryMsg::Base(app::BaseQueryMsg::BaseAdmin {}))?;
+    assert_eq!(admin_res.admin.unwrap(), account.manager.address()?);
+
+    // Can call either by account owner or manager
+    app.call_as(&sender).execute(
+        &mock::ExecuteMsg::Module(MockExecMsg::DoSomethingAdmin {}),
+        None,
+    )?;
+    app.call_as(&account.manager.address()?).execute(
+        &mock::ExecuteMsg::Module(MockExecMsg::DoSomethingAdmin {}),
+        None,
+    )?;
+
+    // Not admin or manager
+    let err: MockError = app
+        .call_as(&Addr::unchecked("who"))
+        .execute(
+            &mock::ExecuteMsg::Module(MockExecMsg::DoSomethingAdmin {}),
+            None,
+        )
+        .unwrap_err()
+        .downcast()
+        .unwrap();
+    assert_eq!(err, MockError::Admin(AdminError::NotAdmin {}));
+    Ok(())
+}
+
+#[test]
+fn subaccount_app_ownership() -> AResult {
+    let sender = Addr::unchecked(common::OWNER);
+    let chain = Mock::new(&sender);
+    let deployment = Abstract::deploy_on(chain.clone(), sender.to_string())?;
+    let account = create_default_account(&deployment.account_factory)?;
+
+    deployment
+        .version_control
+        .claim_namespace(TEST_ACCOUNT_ID, "tester".to_owned())?;
+
+    let app = MockApp::new_test(chain.clone());
+    app.deploy(APP_VERSION.parse().unwrap(), DeployStrategy::Try)?;
+
+    account.manager.create_sub_account(
+        vec![ModuleInstallConfig::new(
+            ModuleInfo::from_id_latest(APP_ID).unwrap(),
+            Some(to_json_binary(&MockInitMsg {}).unwrap()),
+        )],
+        "My subaccount".to_string(),
+        None,
+        None,
+        None,
+        None,
+        &[],
+    )?;
+
+    let sub_account = AbstractAccount::new(&deployment, Some(AccountId::local(2)));
+    let module = sub_account.manager.module_info(APP_ID)?.unwrap();
+    app.set_address(&module.address);
+    let admin_res: AdminResponse =
+        app.query(&mock::QueryMsg::Base(app::BaseQueryMsg::BaseAdmin {}))?;
+    assert_eq!(admin_res.admin.unwrap(), sub_account.manager.address()?);
+    app.call_as(&sender).execute(
+        &mock::ExecuteMsg::Module(MockExecMsg::DoSomethingAdmin {}),
+        None,
+    )?;
     Ok(())
 }
