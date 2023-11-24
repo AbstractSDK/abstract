d8068163e8de54568ce7adec5957bb44a8027678
cyberhoward <cyberhoward@protonmail.com>
2023-11-24 15:54:41 +0100

    formatting

--- framework/packages/abstract-core/src/adapter.rs
+++ framework/packages/abstract-core/src/adapter.rs
@@ -100,7 +100,7 @@ impl<Request: Serialize> AdapterRequestMsg<Request> {
 #[cosmwasm_schema::cw_serde]
 pub struct BaseExecuteMsg {
     /// The Proxy address for which to apply the configuration
-    /// If None, the sender must be an Account manager and the configuration is applied to its associated proxy. 
+    /// If None, the sender must be an Account manager and the configuration is applied to its associated proxy.
     /// If Some, the sender must be a direct or indirect owner (through sub-accounts) of the specified proxy.
     pub proxy_address: Option<String>,
     // The actual base message
