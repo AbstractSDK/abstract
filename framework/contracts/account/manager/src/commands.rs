908dda76c63bb1360d6c0c8d2bb658c910687ec5
AndrewPochapsky <andrew.pochapsky@gmail.com>
2023-11-29 18:46:06 -0500

    undo changes to manager/src/commands.rs

--- framework/contracts/account/manager/src/commands.rs
+++ framework/contracts/account/manager/src/commands.rs
@@ -788,7 +788,6 @@ pub fn update_ibc_status(
     // only owner can update IBC status
     assert_admin_right(deps.as_ref(), &msg_info.sender)?;
     let proxy = ACCOUNT_MODULES.load(deps.storage, PROXY)?;
-    println!("Past proxy");
 
     let maybe_client = ACCOUNT_MODULES.may_load(deps.storage, IBC_CLIENT)?;
 
@@ -816,7 +815,7 @@ fn install_ibc_client(deps: DepsMut, proxy: Addr) -> Result<CosmosMsg, ManagerEr
     let ibc_client_module =
         query_module(deps.as_ref(), ModuleInfo::from_id_latest(IBC_CLIENT)?, None)?;
 
-    let ibc_client_addr = ibc_client_module.module.reference.unwrap_adapter()?;
+    let ibc_client_addr = ibc_client_module.module.reference.unwrap_native()?;
 
     ACCOUNT_MODULES.save(deps.storage, IBC_CLIENT, &ibc_client_addr)?;
 
