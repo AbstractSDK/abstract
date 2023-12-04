ab0e311df9aaf4aae590b6d6c1f3a75e1b51a37b
cyberhoward <cyberhoward@protonmail.com>
2023-12-04 12:04:14 +0100

    rm unused namespace

--- framework/packages/abstract-core/src/objects/common_namespace.rs
+++ framework/packages/abstract-core/src/objects/common_namespace.rs
@@ -1,7 +1,5 @@
 /// namespace key for State Item
 pub const BASE_STATE: &str = "base_state";
-/// namespace for module base state
-pub const CONTRACT_VERSION: &str = "base_state";
 /// namespace for contract Admin
 pub const ADMIN_NAMESPACE: &str = "admin";
 /// storage key for cw_ownable::Ownership
