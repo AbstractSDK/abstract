ec74309ca908440f9120114e468f32289f6ed021
cyberhoward <cyberhoward@protonmail.com>
2023-12-08 12:55:42 +0100

    echo running builds

--- scripts/cancel-wasm.sh
+++ scripts/cancel-wasm.sh
@@ -10,6 +10,8 @@ CIRCLECI_TOKEN=$CCI_TOKEN
 RUNNING_BUILDS=$(curl -s -H "Circle-Token: $CIRCLECI_TOKEN" \
                  "https://circleci.com/api/v2/project/$PROJECT_SLUG/pipeline?branch=main&status=running")
 
+echo $RUNNING_BUILDS
+
 # Iterate over each workflow and get their jobs
 for WORKFLOW_ID in $(echo $WORKFLOWS | jq -r '.items[] | select(.name == "build-and-commit") | .id'); do
     JOBS=$(curl -s -H "Circle-Token: $CIRCLECI_TOKEN" \
