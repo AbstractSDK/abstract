5cc4c9309f92581bc99bb89e1d2d2a9c209db793
AndrewPochapsky <andrew.pochapsky@gmail.com>
2023-12-16 16:17:59 -0500

    fmt

--- modules/contracts/apps/etf/examples/deploy.rs
+++ modules/contracts/apps/etf/examples/deploy.rs
@@ -43,7 +43,11 @@ fn main() -> anyhow::Result<()> {
 
     let args = Arguments::parse();
 
-    let networks = args.network_ids.iter().map(|n| parse_network(n).unwrap()).collect();
+    let networks = args
+        .network_ids
+        .iter()
+        .map(|n| parse_network(n).unwrap())
+        .collect();
 
     deploy_etf(networks)
 }
