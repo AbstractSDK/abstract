98671a038e4e6aeeacc71c3449458a3da9f36811
cyberhoward <cyberhoward@protonmail.com>
2023-12-18 09:50:56 +0100

    add module deployment to migration script

--- interchain/scripts/src/bin/migrate.rs
+++ interchain/scripts/src/bin/migrate.rs
@@ -1,4 +1,10 @@
-use abstract_interface::Abstract;
+use abstract_interface::{Abstract, AppDeployer, DeployStrategy, AdapterDeployer};
+use abstract_cw_staking::{interface::CwStakingAdapter, CW_STAKING_ADAPTER_ID};
+use abstract_dex_adapter::{interface::DexAdapter, msg::DexInstantiateMsg, DEX_ADAPTER_ID};
+use cosmwasm_std::Decimal;
+use dca_app::{contract::DCA_APP_ID, DCA};
+use challenge_app::{contract::CHALLENGE_APP_ID, Challenge};
+use etf_app::{contract::interface::Etf, ETF_APP_ID};
 
 use clap::Parser;
 use cw_orch::{
@@ -21,9 +27,39 @@ fn migrate(networks: Vec<ChainInfo>) -> anyhow::Result<()> {
             .chain(network)
             .build()?;
 
-        let deployment = Abstract::load_from(chain)?;
+        let deployment = Abstract::load_from(chain.clone())?;
 
         deployment.migrate_if_needed()?;
+
+        // Deploy Adapters
+        CwStakingAdapter::new(CW_STAKING_ADAPTER_ID, chain.clone()).deploy(
+            abstract_cw_staking::contract::CONTRACT_VERSION.parse()?,
+            Empty {},
+            DeployStrategy::Try,
+        )?;
+        DexAdapter::new(DEX_ADAPTER_ID, chain.clone()).deploy(
+            abstract_dex_adapter::contract::CONTRACT_VERSION.parse()?,
+            DexInstantiateMsg {
+                recipient_account: 0,
+                swap_fee: Decimal::permille(3),
+            },
+            DeployStrategy::Try,
+        )?;
+
+        // Deploy apps
+        Etf::new(ETF_APP_ID, chain.clone()).deploy(
+            etf_app::contract::CONTRACT_VERSION.parse()?,
+            DeployStrategy::Try,
+        )?;
+
+        DCA::new(DCA_APP_ID, chain.clone()).deploy(
+            dca_app::contract::DCA_APP_VERSION.parse()?,
+            DeployStrategy::Try,
+        )?;
+        Challenge::new(CHALLENGE_APP_ID, chain.clone()).deploy(
+            challenge_app::contract::CHALLENGE_APP_VERSION.parse()?,
+            DeployStrategy::Try,
+        )?;
     }
     Ok(())
 }
