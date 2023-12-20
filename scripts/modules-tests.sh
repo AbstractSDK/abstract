ce541b50171216045ceba50e0ac92fc31496c185
Buckram <buckram123@gmail.com>
2023-12-20 13:40:37 +0200

    install golang and use nextest

---
+++ scripts/modules-tests.sh
@@ -0,0 +1,35 @@
+# go into the directory we want to compile
+cd ./modules
+
+# Create lock file if it does not exist
+if [ ! -f Cargo.lock ]; then
+  cargo generate-lockfile
+fi
+
+sudo apt-get update && sudo apt-get install libclang-dev -y
+
+# Set Go version
+GO_VERSION="1.18"
+
+# Download Go
+wget https://dl.google.com/go/go${GO_VERSION}.linux-amd64.tar.gz -O /tmp/go${GO_VERSION}.linux-amd64.tar.gz
+
+# Extract Go archive
+sudo tar -C /usr/local -xzf /tmp/go${GO_VERSION}.linux-amd64.tar.gz
+
+# Set environment variables
+echo "export GOROOT=/usr/local/go" >> ~/.bash_profile
+echo "export GOPATH=$HOME/go" >> ~/.bash_profile
+echo "export PATH=$PATH:/usr/local/go/bin:$GOPATH/bin" >> ~/.bash_profile
+
+# Install nextest
+curl -LsSf https://get.nexte.st/latest/linux | tar zxf - -C ${CARGO_HOME:-~/.cargo}/bin
+
+# Load the environment variables
+source ~/.bash_profile
+
+# Check the installed versions
+go version
+cargo nextest -V
+
+cargo nextest run --locked --all-features --all-targets --build-jobs 2
