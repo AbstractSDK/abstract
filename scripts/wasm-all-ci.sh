8fcba7c8d272571781229fa94f86621ba35f7897
Buckram <buckram123@gmail.com>
2023-12-21 21:18:01 +0200

    don't cache wasm builds

--- scripts/wasm-all-ci.sh
+++ scripts/wasm-all-ci.sh
@@ -4,13 +4,13 @@ set -e
 
 # Detect the architecture #
 if [[ $(arch) == "arm64" ]]; then
-    image="cosmwasm/rust-optimizer-arm64"
-    workspace_image="cosmwasm/workspace-optimizer-arm64"
-    abstract_image="abstractmoney/workspace-optimizer-arm64"
+image="cosmwasm/rust-optimizer-arm64"
+workspace_image="cosmwasm/workspace-optimizer-arm64"
+abstract_image="abstractmoney/workspace-optimizer-arm64"
 else
-    image="cosmwasm/rust-optimizer"
-    workspace_image="cosmwasm/workspace-optimizer"
-    abstract_image="abstractmoney/workspace-optimizer"
+image="cosmwasm/rust-optimizer"
+workspace_image="cosmwasm/workspace-optimizer"
+abstract_image="abstractmoney/workspace-optimizer"
 fi
 
 starting_dir=$(pwd)
@@ -29,34 +29,23 @@ rm packages/abstract-interface/artifacts || true
 
 # Create lock file if it does not exist
 if [ ! -f Cargo.lock ]; then
-    cargo generate-lockfile
+  cargo generate-lockfile
 fi
 
 docker rm -v with_code || true
-docker rm -v build_with_code || true
 
 # create a dummy container which will hold a volume with config
-docker create -v /code -v /target/wasm32-unknown-unknown/release --name with_code alpine /bin/true
+docker create -v /code --name with_code alpine /bin/true
 # copy a config file into this volume
 docker cp Cargo.toml with_code:/code
 docker cp Cargo.lock with_code:/code
 # copy code into this volume
 docker cp ./contracts with_code:/code
 docker cp ./packages with_code:/code
-# Copy cache if any
-docker cp ./target/wasm32-unknown-unknown/release/.fingerprint with_code:/target/wasm32-unknown-unknown/release/ || true
-docker cp ./target/wasm32-unknown-unknown/release/build with_code:/target/wasm32-unknown-unknown/release/ || true
-docker cp ./target/wasm32-unknown-unknown/release/deps with_code:/target/wasm32-unknown-unknown/release/ || true
-docker cp ~/.cargo/registry with_code:/usr/local/cargo/registry || true
 # Run the build
-docker run --name build_with_code --volumes-from with_code ${abstract_image}:0.15.0
+docker run --volumes-from with_code ${abstract_image}:0.15.0
 # Copy the artifacts back out
 docker cp with_code:/code/artifacts/ .
-# Copy cache back out
-mkdir -p ./target/wasm32-unknown-unknown/release
-docker cp build_with_code:/target/wasm32-unknown-unknown/release/.fingerprint ./target/wasm32-unknown-unknown/release
-docker cp build_with_code:/target/wasm32-unknown-unknown/release/build ./target/wasm32-unknown-unknown/release
-docker cp build_with_code:/target/wasm32-unknown-unknown/release/deps ./target/wasm32-unknown-unknown/release
 ls artifacts
 
 cd $starting_dir
@@ -64,10 +53,9 @@ cd $starting_dir
 echo "Wasming modules"
 
 docker rm -v modules_with_code || true
-docker rm -v build_modules_with_code || true
 
 # create a dummy container which will hold a volume with config
-docker create -v /code -v /integrations -v /framework -v /target/wasm32-unknown-unknown/release --name modules_with_code alpine /bin/true
+docker create -v /code -v /integrations -v /framework --name modules_with_code alpine /bin/true
 
 # copy directories to container.
 docker cp ./integrations modules_with_code:/
@@ -78,7 +66,7 @@ cd ./modules
 
 # Create lock file if it does not exist
 if [ ! -f Cargo.lock ]; then
-    cargo generate-lockfile
+  cargo generate-lockfile
 fi
 
 # Delete the current artifacts folder.
@@ -86,22 +74,12 @@ fi
 # copy a config file into this volume
 docker cp Cargo.toml modules_with_code:/code
 docker cp Cargo.lock modules_with_code:/code
-# Copy cache if any
-docker cp ./target/wasm32-unknown-unknown/release/.fingerprint modules_with_code:/target/wasm32-unknown-unknown/release/ || true
-docker cp ./target/wasm32-unknown-unknown/release/build modules_with_code:/target/wasm32-unknown-unknown/release/ || true
-docker cp ./target/wasm32-unknown-unknown/release/deps modules_with_code:/target/wasm32-unknown-unknown/release/ || true
-docker cp ~/.cargo/registry modules_with_code:/usr/local/cargo/registry || true
 # copy code into this volume
 docker cp ./contracts modules_with_code:/code
 
 # Run the build
-docker run --name build_modules_with_code --volumes-from modules_with_code ${abstract_image}:0.15.0
+docker run --volumes-from modules_with_code ${abstract_image}:0.15.0
 # Copy the artifacts back out
 docker cp modules_with_code:/code/artifacts/ .
-# Copy cache back out
-mkdir -p ./target/wasm32-unknown-unknown/release
-docker cp build_modules_with_code:/target/wasm32-unknown-unknown/release/.fingerprint ./target/wasm32-unknown-unknown/release
-docker cp build_modules_with_code:/target/wasm32-unknown-unknown/release/build ./target/wasm32-unknown-unknown/release
-docker cp build_modules_with_code:/target/wasm32-unknown-unknown/release/deps ./target/wasm32-unknown-unknown/release
 ls artifacts
 cd $starting_dir
