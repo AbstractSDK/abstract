1b7a037742b7be8b10f911b4c1063bcfaecd40a5
Buckram <buckram123@gmail.com>
2023-12-21 18:34:04 +0200

    remove debug related comment

--- framework/packages/abstract-adapter/src/endpoints/execute.rs
+++ framework/packages/abstract-adapter/src/endpoints/execute.rs
@@ -177,8 +177,6 @@ impl<Error: ContractError, CustomInitMsg, CustomExecMsg, CustomQueryMsg, Receive
 
         // Handle the addition of authorized addresses
         for authorized in to_add {
-            // It's called by the manager but sender is sender of the manager here
-
             // authorized here can either be a contract address or a module id
             let authorized_addr = get_addr_from_module_id_or_addr(
                 deps.as_ref(),
