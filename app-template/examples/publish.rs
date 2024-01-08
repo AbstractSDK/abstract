7646fb5c771685f8eaec912b1852e6aa9145ef2c
cyberhoward <cyberhoward@protonmail.com>
2024-01-08 12:38:54 +0100

    format

--- app-template/examples/publish.rs
+++ app-template/examples/publish.rs
@@ -41,9 +41,7 @@ fn deploy(networks: Vec<ChainInfo>) -> anyhow::Result<()> {
         let abstract_client: AbstractClient<Daemon> = AbstractClient::new(chain.clone())?;
 
         // Get the [`Publisher`] that owns the namespace, otherwise create a new one and claim the namespace
-        let publisher: Publisher<_> = abstract_client
-            .publisher_builder(app_namespace)
-            .build()?;
+        let publisher: Publisher<_> = abstract_client.publisher_builder(app_namespace).build()?;
 
         if publisher.account().owner()? != chain.sender() {
             panic!("The current sender can not publish to this namespace. Please use the wallet that owns the Account that owns the Namespace.")
