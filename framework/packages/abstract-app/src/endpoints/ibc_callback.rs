d35703cce6b72ae9561dfcb9d4c2e628cceed8de
cyberhoward <cyberhoward@protonmail.com>
2024-02-16 15:10:29 -0800

    fix Add import in abstract-app IBC callback

--- framework/packages/abstract-app/src/endpoints/ibc_callback.rs
+++ framework/packages/abstract-app/src/endpoints/ibc_callback.rs
@@ -1,5 +1,6 @@
 use abstract_core::IBC_CLIENT;
 use abstract_sdk::ModuleInterface;
+use cosmwasm_std::Addr;
 
 use crate::{state::ContractError, AppContract, IbcCallbackEndpoint};
 
@@ -22,7 +23,7 @@ impl<
         SudoMsg,
     >
 {
-    fn ibc_client(&self, deps: cosmwasm_std::Deps) -> Result<cw_orch::prelude::Addr, Self::Error> {
+    fn ibc_client(&self, deps: cosmwasm_std::Deps) -> Result<Addr, Self::Error> {
         let ibc_client = self.modules(deps).module_address(IBC_CLIENT)?;
         Ok(ibc_client)
     }
