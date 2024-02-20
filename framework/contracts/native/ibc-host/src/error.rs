88eca6836589e4a2a2454c612723826050114077
Buckram <buckram123@gmail.com>
2024-02-20 19:48:08 +0200

    add migrate logic to ibc-host

--- framework/contracts/native/ibc-host/src/error.rs
+++ framework/contracts/native/ibc-host/src/error.rs
@@ -44,8 +44,8 @@ pub enum HostError {
     ProxyAddressExists {},
 }
 
-impl From<semver::Error> for HostError {
-    fn from(err: semver::Error) -> Self {
+impl From<cw_semver::Error> for HostError {
+    fn from(err: cw_semver::Error) -> Self {
         Self::SemVer(err.to_string())
     }
 }
