b4e9bd01d3458e540581c43fb1878284859a2da6
Buckram <buckram123@gmail.com>
2024-03-05 15:01:12 +0200

    temporary fix

--- interchain/tests/src/clone_testing/common.rs
+++ interchain/tests/src/clone_testing/common.rs
@@ -1,6 +1,6 @@
 use abstract_client::{AbstractClient, Environment};
 use abstract_interface::{Abstract, VCExecFns};
-use cosmwasm_std::Addr;
+use cosmwasm_std::{coins, Addr};
 use cw_orch::prelude::*;
 use cw_orch::{contract::Deploy, daemon::ChainInfo};
 use cw_orch_clone_testing::CloneTesting;
@@ -40,7 +40,10 @@ pub fn load_abstr(chain: ChainInfo, sender: Addr) -> anyhow::Result<AbstractClie
     let _ = env_logger::builder().is_test(true).try_init();
     // We set the state file to be able to clone test
     std::env::set_var("STATE_FILE", "../scripts/state.json");
+    let gas_denom = chain.gas_denom;
     let mut app = CloneTesting::new(rt(), chain)?;
+    // Make sure sender have enough gas
+    app.add_balance(&sender, coins(1_000_000_000_000, gas_denom))?;
     app.set_sender(sender);
 
     let abstr_deployment = AbstractClient::new(app)?;
--- interchain/tests/src/clone_testing/staking/astroport.rs
+++ interchain/tests/src/clone_testing/staking/astroport.rs
@@ -1,25 +1,24 @@
-use abstract_app::objects::{
-    pool_id::PoolAddressBase, AssetEntry, LpToken, PoolMetadata, PoolType, UncheckedContractEntry,
-};
-use abstract_client::{AbstractClient, Environment};
+use abstract_client::Environment;
 use abstract_cw_staking::staking_tester::{MockStaking, StakingTester};
-use abstract_interface::ExecuteMsgFns;
-use cosmwasm_std::{coin, Addr};
+use cosmwasm_std::Addr;
 use cw_asset::AssetInfoUnchecked;
 use cw_orch::daemon::networks::NEUTRON_1;
 use cw_orch::prelude::*;
 use cw_orch_clone_testing::CloneTesting;
-
-use crate::clone_testing::dex::astroport::{FACTORY_ADDR, GENERATOR_ADDR};
 
 use super::load_abstr;
 
 // mainnet addr of abstract
 const SENDER: &str = "neutron1kjzpqv393k4g064xh04j4hwy5d0s03wf7dnt4x";
 
-const ASSET_A: &str = "test-asset-one";
-const ASSET_B: &str = "test-asset-two";
-const ASSET_AMOUNT: u128 = 1_000_000_000_000;
+// const ASSET_A: &str = "test-asset-one";
+// const ASSET_B: &str = "test-asset-two";
+// const ASSET_AMOUNT: u128 = 1_000_000_000_000;
+
+// Using neutron-atom pool, new Astroport pair contract address breaks clone-testing for some reason
+// https://github.com/AbstractSDK/abstract-name-service/blob/52b626b54f449fb16a7d4759aa33b028337fc664/out/assets.json#L1913-L1916
+const ASSET_ENTRY_LP: &str = "astroport/cosmoshub>atom,neutron>ntrn";
+const LP_TOKEN_ADDR: &str = "neutron1jkcf80nd4pfc2krce3xk9m9y994pllq58avx89sfzqlalej4frus27ms3a";
 
 pub struct AstroportStake {
     chain: CloneTesting,
@@ -33,104 +32,119 @@ impl AstroportStake {
     }
 
     fn new(chain: CloneTesting) -> anyhow::Result<Self> {
-        let (ans_asset_a, asset_a) = (
-            "tao".to_owned(),
-            AssetInfoUnchecked::Native(ASSET_A.to_owned()),
-        );
-        let (ans_asset_b, asset_b) = (
-            "tat".to_owned(),
-            AssetInfoUnchecked::Native(ASSET_B.to_owned()),
-        );
-        let asset_a_astroport = astroport::asset::AssetInfo::native(ASSET_A);
-        let asset_b_astroport = astroport::asset::AssetInfo::native(ASSET_B);
-
-        // Create pool
-        let asset_infos = vec![asset_a_astroport.clone(), asset_b_astroport.clone()];
-        let resp = chain.execute(
-            &astroport::factory::ExecuteMsg::CreatePair {
-                pair_type: astroport::factory::PairType::Xyk {},
-                asset_infos,
-                init_params: None,
-            },
-            &[],
-            &Addr::unchecked(FACTORY_ADDR),
-        )?;
-        let pair_contract_addr = resp.event_attr_value("wasm", "pair_contract_addr")?;
-        let liquidity_token_addr = resp.event_attr_value("wasm", "liquidity_token_addr")?;
-
-        let pair_addr = Addr::unchecked(pair_contract_addr);
-
-        // Add some liquidity
-        let assets = vec![
-            astroport::asset::Asset::new(asset_a_astroport, ASSET_AMOUNT),
-            astroport::asset::Asset::new(asset_b_astroport, ASSET_AMOUNT),
-        ];
-        let amount = vec![coin(ASSET_AMOUNT, ASSET_A), coin(ASSET_AMOUNT, ASSET_B)];
-        chain.add_balance(&chain.sender, amount.clone())?;
-        chain.execute(
-            &astroport::pair::ExecuteMsg::ProvideLiquidity {
-                assets,
-                slippage_tolerance: None,
-                auto_stake: None,
-                receiver: None,
-            },
-            &amount,
-            &pair_addr,
-        )?;
-
-        let pool = PoolAddressBase::Contract(pair_addr.to_string());
-        let pool_metadata = PoolMetadata {
-            dex: Self::name(),
-            pool_type: PoolType::ConstantProduct,
-            assets: vec![AssetEntry::new(&ans_asset_a), AssetEntry::new(&ans_asset_b)],
-        };
-        let lp_asset = AssetInfoUnchecked::Cw20(liquidity_token_addr);
-
-        // Register everything on ans
-        let abstr_deployment = AbstractClient::new(chain.clone())?;
-        abstr_deployment.name_service().update_contract_addresses(
-            vec![(
-                UncheckedContractEntry {
-                    protocol: Self::name(),
-                    contract: format!(
-                        "staking/{dex}/{asset_a},{asset_b}",
-                        dex = Self::name(),
-                        asset_a = &ans_asset_a,
-                        asset_b = &ans_asset_b,
-                    ),
-                },
-                GENERATOR_ADDR.to_owned(),
-            )],
-            vec![],
-        )?;
-        // Add assets
-        abstr_deployment.name_service().update_asset_addresses(
-            vec![
-                (ans_asset_a.clone(), asset_a),
-                (ans_asset_b.clone(), asset_b),
-            ],
-            vec![],
-        )?;
-        // Add dex
-        abstr_deployment
-            .name_service()
-            .update_dexes(vec![Self::name()], vec![])?;
-        // Add pool
-        abstr_deployment
-            .name_service()
-            .update_pools(vec![(pool, pool_metadata)], vec![])?;
-        // Add lp asset
-        let lp_token = LpToken::new(Self::name(), vec![ans_asset_a, ans_asset_b]);
-        abstr_deployment
-            .name_service()
-            .update_asset_addresses(vec![(lp_token.to_string(), lp_asset.clone())], vec![])?;
-
+        let lp_token_addr = Addr::unchecked(LP_TOKEN_ADDR);
+        let minter_response: cw20::MinterResponse = chain
+            .wasm_querier()
+            .smart_query(&lp_token_addr, &cw20::Cw20QueryMsg::Minter {})?;
         Ok(Self {
             chain,
-            lp_asset: (lp_token.to_string(), lp_asset),
-            minter: pair_addr,
+            lp_asset: (
+                ASSET_ENTRY_LP.to_owned(),
+                AssetInfoUnchecked::cw20(lp_token_addr.to_string()),
+            ),
+            minter: Addr::unchecked(minter_response.minter),
         })
     }
+    // TODO: fix if possible, tests would be more stable using our assets
+    // fn new(chain: CloneTesting) -> anyhow::Result<Self> {
+    //     let (ans_asset_a, asset_a) = (
+    //         "tao".to_owned(),
+    //         AssetInfoUnchecked::Native(ASSET_A.to_owned()),
+    //     );
+    //     let (ans_asset_b, asset_b) = (
+    //         "tat".to_owned(),
+    //         AssetInfoUnchecked::Native(ASSET_B.to_owned()),
+    //     );
+    //     let asset_a_astroport = astroport::asset::AssetInfo::native(ASSET_A);
+    //     let asset_b_astroport = astroport::asset::AssetInfo::native(ASSET_B);
+
+    //     // Create pool
+    //     let asset_infos = vec![asset_a_astroport.clone(), asset_b_astroport.clone()];
+    //     let resp = chain.execute(
+    //         &astroport::factory::ExecuteMsg::CreatePair {
+    //             pair_type: astroport::factory::PairType::Xyk {},
+    //             asset_infos,
+    //             init_params: None,
+    //         },
+    //         &[],
+    //         &Addr::unchecked(FACTORY_ADDR),
+    //     )?;
+    //     let pair_contract_addr = resp.event_attr_value("wasm", "pair_contract_addr")?;
+    //     let liquidity_token_addr = resp.event_attr_value("wasm", "liquidity_token_addr")?;
+
+    //     let pair_addr = Addr::unchecked(pair_contract_addr);
+
+    //     // Add some liquidity
+    //     let assets = vec![
+    //         astroport::asset::Asset::new(asset_a_astroport, ASSET_AMOUNT),
+    //         astroport::asset::Asset::new(asset_b_astroport, ASSET_AMOUNT),
+    //     ];
+    //     let amount = vec![coin(ASSET_AMOUNT, ASSET_A), coin(ASSET_AMOUNT, ASSET_B)];
+    //     chain.add_balance(&chain.sender, amount.clone())?;
+    //     chain.execute(
+    //         &astroport::pair::ExecuteMsg::ProvideLiquidity {
+    //             assets,
+    //             slippage_tolerance: None,
+    //             auto_stake: None,
+    //             receiver: None,
+    //         },
+    //         &amount,
+    //         &pair_addr,
+    //     )?;
+
+    //     let pool = PoolAddressBase::Contract(pair_addr.to_string());
+    //     let pool_metadata = PoolMetadata {
+    //         dex: Self::name(),
+    //         pool_type: PoolType::ConstantProduct,
+    //         assets: vec![AssetEntry::new(&ans_asset_a), AssetEntry::new(&ans_asset_b)],
+    //     };
+    //     let lp_asset = AssetInfoUnchecked::Cw20(liquidity_token_addr);
+
+    //     // Register everything on ans
+    //     let abstr_deployment = AbstractClient::new(chain.clone())?;
+    //     abstr_deployment.name_service().update_contract_addresses(
+    //         vec![(
+    //             UncheckedContractEntry {
+    //                 protocol: Self::name(),
+    //                 contract: format!(
+    //                     "staking/{dex}/{asset_a},{asset_b}",
+    //                     dex = Self::name(),
+    //                     asset_a = &ans_asset_a,
+    //                     asset_b = &ans_asset_b,
+    //                 ),
+    //             },
+    //             GENERATOR_ADDR.to_owned(),
+    //         )],
+    //         vec![],
+    //     )?;
+    //     // Add assets
+    //     abstr_deployment.name_service().update_asset_addresses(
+    //         vec![
+    //             (ans_asset_a.clone(), asset_a),
+    //             (ans_asset_b.clone(), asset_b),
+    //         ],
+    //         vec![],
+    //     )?;
+    //     // Add dex
+    //     abstr_deployment
+    //         .name_service()
+    //         .update_dexes(vec![Self::name()], vec![])?;
+    //     // Add pool
+    //     abstr_deployment
+    //         .name_service()
+    //         .update_pools(vec![(pool, pool_metadata)], vec![])?;
+    //     // Add lp asset
+    //     let lp_token = LpToken::new(Self::name(), vec![ans_asset_a, ans_asset_b]);
+    //     abstr_deployment
+    //         .name_service()
+    //         .update_asset_addresses(vec![(lp_token.to_string(), lp_asset.clone())], vec![])?;
+
+    //     Ok(Self {
+    //         chain,
+    //         lp_asset: (lp_token.to_string(), lp_asset),
+    //         minter: pair_addr,
+    //     })
+    // }
 }
 
 impl MockStaking for AstroportStake {
