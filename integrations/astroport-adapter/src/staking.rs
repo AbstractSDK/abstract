058eb06ba59d8c04f75762951f5b1ddc87457c2b
Mykhailo Donchenko <91957742+Buckram123@users.noreply.github.com>
2024-03-14 16:02:00 +0200

    Generic dex and staking testing with Astroport and kujira testing (#270)
    * init
    
    * astroport native swap
    
    * move dextester to dex adapter
    
    * provide liquidity tests
    
    * cw20 tests
    
    * Update migration tests to clone tests
    
    * format
    
    * format interchain
    
    * remove Abstract object usage, where possible
    
    * spread tests
    
    * kujira dex tests
    
    * astroport generator fails to deserialize factory response
    
    * fmt
    
    * temporary fix
    
    * astroport staking tests and fixes
    
    * enable debug
    
    * remove first swap
    
    * kujira magically fixed
    
    * add slippage tests
    
    * kujira staking tests
    
    * fix slippage tests for astroport
    
    * restructure interchain
    
    * run interchain tests when modules or frameworks changed
    
    * cleanup
    
    * review fixes
    
    * weird compile error
    
    * state file not created by default by daemonstate
    
    * clone-test requires existing state file
    
    * add status docs to adapters
    
    * test dex queries
    
    ---------
    
    Co-authored-by: cyberhoward <cyberhoward@protonmail.com>

--- integrations/astroport-adapter/src/staking.rs
+++ integrations/astroport-adapter/src/staking.rs
@@ -13,12 +13,12 @@ pub struct Astroport {
 pub struct AstroportTokenContext {
     pub lp_token: LpToken,
     pub lp_token_address: Addr,
-    pub generator_contract_address: Addr,
+    pub incentives_contract_address: Addr,
 }
 
 // Data that's retrieved from ANS
 // - LP token address, based on provided LP token
-// - Generator address = staking_address
+// - Incentives address = staking_address
 impl Identify for Astroport {
     fn name(&self) -> &'static str {
         ASTROPORT
@@ -39,9 +39,8 @@ use ::{
         RewardTokensResponse, StakeResponse, StakingInfo, StakingInfoResponse, UnbondingResponse,
     },
     abstract_staking_standard::{CwStakingCommand, CwStakingError},
-    astroport::generator::{
-        Config, Cw20HookMsg, ExecuteMsg as GeneratorExecuteMsg, QueryMsg as GeneratorQueryMsg,
-        RewardInfoResponse,
+    astroport::incentives::{
+        Config, Cw20Msg, ExecuteMsg as IncentivesExecuteMsg, QueryMsg as IncentivesQueryMsg,
     },
     cosmwasm_std::{
         to_json_binary, wasm_execute, CosmosMsg, Deps, Env, QuerierWrapper, StdError, Uint128,
@@ -65,7 +64,7 @@ impl CwStakingCommand for Astroport {
         self.tokens = lp_tokens
             .into_iter()
             .map(|entry| {
-                let generator_contract_address =
+                let incentives_contract_address =
                     self.staking_contract_address(deps, ans_host, &entry)?;
 
                 let AssetInfo::Cw20(token_addr) = entry.resolve(&deps.querier, ans_host)? else {
@@ -80,7 +79,7 @@ impl CwStakingCommand for Astroport {
                 Ok(AstroportTokenContext {
                     lp_token,
                     lp_token_address,
-                    generator_contract_address,
+                    incentives_contract_address,
                 })
             })
             .collect::<Result<_, CwStakingError>>()?;
@@ -93,7 +92,7 @@ impl CwStakingCommand for Astroport {
         stake_request: Vec<AnsAsset>,
         _unbonding_period: Option<cw_utils::Duration>,
     ) -> Result<Vec<CosmosMsg>, CwStakingError> {
-        let msg = to_json_binary(&Cw20HookMsg::Deposit {})?;
+        let msg = to_json_binary(&Cw20Msg::Deposit { recipient: None })?;
 
         let stake_msgs = stake_request
             .into_iter()
@@ -102,7 +101,7 @@ impl CwStakingCommand for Astroport {
                 let msg: CosmosMsg = wasm_execute(
                     token.lp_token_address.to_string(),
                     &Cw20ExecuteMsg::Send {
-                        contract: token.generator_contract_address.to_string(),
+                        contract: token.incentives_contract_address.to_string(),
                         amount: stake.amount,
                         msg: msg.clone(),
                     },
@@ -127,8 +126,8 @@ impl CwStakingCommand for Astroport {
             .zip(self.tokens.iter())
             .map(|(unstake, token)| {
                 let msg: CosmosMsg = wasm_execute(
-                    token.generator_contract_address.to_string(),
-                    &GeneratorExecuteMsg::Withdraw {
+                    token.incentives_contract_address.to_string(),
+                    &IncentivesExecuteMsg::Withdraw {
                         lp_token: token.lp_token_address.to_string(),
                         amount: unstake.amount,
                     },
@@ -149,7 +148,7 @@ impl CwStakingCommand for Astroport {
         let mut claims: HashMap<&str, Vec<String>> = HashMap::new();
         for token in &self.tokens {
             claims
-                .entry(token.generator_contract_address.as_str())
+                .entry(token.incentives_contract_address.as_str())
                 .and_modify(|tokens| tokens.push(token.lp_token_address.to_string()))
                 .or_insert(vec![token.lp_token_address.to_string()]);
         }
@@ -158,7 +157,7 @@ impl CwStakingCommand for Astroport {
             .map(|(generator_addr, lp_tokens)| {
                 let msg: CosmosMsg = wasm_execute(
                     generator_addr.to_owned(),
-                    &GeneratorExecuteMsg::ClaimRewards { lp_tokens },
+                    &IncentivesExecuteMsg::ClaimRewards { lp_tokens },
                     vec![],
                 )?
                 .into();
@@ -172,13 +171,13 @@ impl CwStakingCommand for Astroport {
         let generator_addrs: HashSet<&Addr> = self
             .tokens
             .iter()
-            .map(|t| &t.generator_contract_address)
+            .map(|t| &t.incentives_contract_address)
             .collect();
 
         let mut infos = Vec::with_capacity(generator_addrs.len());
         for g_addr in generator_addrs {
             let Config { astro_token, .. } = querier
-                .query_wasm_smart::<Config>(g_addr.clone(), &GeneratorQueryMsg::Config {})
+                .query_wasm_smart::<Config>(g_addr.clone(), &IncentivesQueryMsg::Config {})
                 .map_err(|e| {
                     StdError::generic_err(format!(
                         "Failed to query staking info for {} with generator: {}, {:?}",
@@ -219,8 +218,8 @@ impl CwStakingCommand for Astroport {
             .map(|t| {
                 let stake_balance: Uint128 = querier
                     .query_wasm_smart(
-                        t.generator_contract_address.clone(),
-                        &GeneratorQueryMsg::Deposit {
+                        t.incentives_contract_address.clone(),
+                        &IncentivesQueryMsg::Deposit {
                             lp_token: t.lp_token_address.to_string(),
                             user: staker.to_string(),
                         },
@@ -256,10 +255,10 @@ impl CwStakingCommand for Astroport {
             .tokens
             .iter()
             .map(|t| {
-                let reward_info: RewardInfoResponse = querier
+                let reward_infos: Vec<astroport::incentives::RewardInfo> = querier
                     .query_wasm_smart(
-                        t.generator_contract_address.clone(),
-                        &GeneratorQueryMsg::RewardInfo {
+                        t.incentives_contract_address.clone(),
+                        &IncentivesQueryMsg::RewardInfo {
                             lp_token: t.lp_token_address.to_string(),
                         },
                     )
@@ -272,18 +271,21 @@ impl CwStakingCommand for Astroport {
                         ))
                     })?;
 
-                let token = match reward_info.base_reward_token {
-                    astroport::asset::AssetInfo::Token { contract_addr } => {
-                        AssetInfo::cw20(contract_addr)
-                    }
-                    astroport::asset::AssetInfo::NativeToken { denom } => AssetInfo::native(denom),
-                };
+                let tokens = reward_infos
+                    .into_iter()
+                    .map(|info| match info.reward {
+                        astroport::incentives::RewardType::Int(info)
+                        | astroport::incentives::RewardType::Ext { info, .. } => match info {
+                            astroport::asset::AssetInfo::Token { contract_addr } => {
+                                AssetInfo::cw20(contract_addr)
+                            }
+                            astroport::asset::AssetInfo::NativeToken { denom } => {
+                                AssetInfo::native(denom)
+                            }
+                        },
+                    })
+                    .collect();
 
-                let mut tokens = vec![token];
-
-                if let Some(reward_token) = reward_info.proxy_reward_token {
-                    tokens.push(AssetInfo::cw20(reward_token));
-                }
                 Ok(tokens)
             })
             .collect::<Result<_, CwStakingError>>()?;
