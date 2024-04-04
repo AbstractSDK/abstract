340879033bead3b8c4d740510c0904c645ea3885
CyberHoward <88450409+CyberHoward@users.noreply.github.com>
2024-04-04 12:50:42 +0200

    Remove template from monorepo (#307)
    * remove app template from monorepo
    
    * rm uses of app-template in ci

--- app-template/src/replies/instantiate.rs
+++
@@ -1,8 +0,0 @@
-use crate::contract::{App, AppResult};
-
-use abstract_app::traits::AbstractResponse;
-use cosmwasm_std::{DepsMut, Env, Reply};
-
-pub fn instantiate_reply(_deps: DepsMut, _env: Env, app: App, _reply: Reply) -> AppResult {
-    Ok(app.response("instantiate_reply"))
-}
