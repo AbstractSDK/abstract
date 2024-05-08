e31e0b40a5c1c1c090589107617f20803ae6599c
Buckram <buckram123@gmail.com>
2024-05-08 22:08:54 +0300

    last fix for publish script

--- framework/publish/publish.sh
+++ framework/publish/publish.sh
@@ -113,4 +113,4 @@ for pack in $STANDARDS; do
 done
 
 VERSION=$(grep -A1 "\[workspace.package\]" framework/Cargo.toml | awk -F'"' '/version/ {print $2}');
-sh ./publish/tag-release.sh "v$VERSION"
+sh ./framework/publish/tag-release.sh "v$VERSION"
