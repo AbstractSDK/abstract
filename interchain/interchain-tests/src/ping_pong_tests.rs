56067992147e9dae8c458d62aec215ba850cc72e
Buckram <buckram123@gmail.com>
2024-05-16 11:28:06 +0300

    pingpong tests init

--- interchain/interchain-tests/src/ping_pong_tests.rs
+++ interchain/interchain-tests/src/ping_pong_tests.rs
@@ -1,56 +1,68 @@
+#![cfg(test)]
+
 use abstract_app::objects::namespace::Namespace;
 use abstract_app::objects::AccountId;
 
 use abstract_client::Application;
 use abstract_client::{AbstractClient, Environment};
-use abstract_cw_orch_polytone::Polytone;
 
+use abstract_std::objects::chain_name::ChainName;
 // Use prelude to get all the necessary imports
 use cw_orch::{anyhow, prelude::*};
 
-use abstract_interchain_tests::setup::ibc_connect_polytone_and_abstract;
+use crate::setup::mock_test::logger_test_init;
+use crate::setup::{ibc_abstract_setup, ibc_connect_polytone_and_abstract};
+use crate::{JUNO, STARGAZE};
 use ping_pong::contract::APP_ID;
 use ping_pong::msg::{AppInstantiateMsg, PongsResponse};
 use ping_pong::{AppExecuteMsgFns, AppInterface, AppQueryMsgFns};
 
-struct PingPong<Env: CwEnv> {
-    abs: AbstractClient<Env>,
+struct PingPong<Env: IbcQueryHandler> {
+    abs_juno: AbstractClient<Env>,
+    abs_stargaze: AbstractClient<Env>,
     app1: Application<Env, AppInterface<Env>>,
     app2: Application<Env, AppInterface<Env>>,
 }
 
-impl<Env: CwEnv> PingPong<Env> {
+impl PingPong<MockBech32> {
     /// Set up the test environment with two Accounts that has the App installed
-    fn setup(env: Env) -> anyhow::Result<PingPong<Env>> {
+    fn setup() -> anyhow::Result<PingPong<MockBech32>> {
+        let mock_interchain =
+            MockBech32InterchainEnv::new(vec![(JUNO, "juno"), (STARGAZE, "stargaze")]);
+
+        let mock_juno = mock_interchain.chain(JUNO).unwrap();
+        let mock_stargaze = mock_interchain.chain(STARGAZE).unwrap();
+
+        let abs_juno = AbstractClient::builder(mock_juno).build()?;
+        let abs_stargaze = AbstractClient::builder(mock_stargaze).build()?;
+
         let namespace = Namespace::from_id(APP_ID)?;
-
-        // You can set up Abstract with a builder.
-        let abs_client = AbstractClient::builder(env.clone()).build()?;
-
-        // Publish both the client and the server
-        let publisher = abs_client.publisher_builder(namespace).build()?;
+        // Publish and install on both chains
+        let publisher = abs_juno.publisher_builder(namespace.clone()).build()?;
         publisher.publish_app::<AppInterface<_>>()?;
+        // TODO: https://github.com/AbstractSDK/abstract/pull/346
+        // let app1 = publisher
+        //     .account()
+        //     .install_app_with_dependencies::<AppInterface<_>>(
+        //         &AppInstantiateMsg {},
+        //         Empty {},
+        //         &[],
+        //     )?;
+        abs_juno.account_builder().build();
 
-        let app1 = publisher
-            .account()
-            .install_app_with_dependencies::<AppInterface<_>>(
-                &AppInstantiateMsg {},
-                Empty {},
-                &[],
-            )?;
-
-        let app2 = publisher
-            .account()
-            .install_app_with_dependencies::<AppInterface<_>>(
-                &AppInstantiateMsg {},
-                Empty {},
-                &[],
-            )?;
-
-        Polytone::deploy_on(env, None)?;
+        let publisher = abs_stargaze.publisher_builder(namespace).build()?;
+        publisher.publish_app::<AppInterface<_>>()?;
+        // let app2 = publisher
+        //     .account()
+        //     .install_app_with_dependencies::<AppInterface<_>>(
+        //         &AppInstantiateMsg {},
+        //         Empty {},
+        //         &[],
+        //     )?;
 
         Ok(PingPong {
-            abs: abs_client,
+            abs_juno,
+            abs_stargaze,
             app1,
             app2,
         })
@@ -59,24 +71,27 @@ impl<Env: CwEnv> PingPong<Env> {
 
 #[test]
 fn successful_install() -> anyhow::Result<()> {
+    logger_test_init();
     // Create a sender and mock env
-    let mock = MockBech32::new("mock");
-    let env = PingPong::setup(mock)?;
+    let env = PingPong::setup()?;
     let app1 = env.app1;
+    let app2 = env.app2;
 
     let pongs = app1.pongs()?;
+    assert_eq!(pongs, PongsResponse { pongs: 0 });
+
+    let pongs = app2.pongs()?;
     assert_eq!(pongs, PongsResponse { pongs: 0 });
     Ok(())
 }
 
 #[test]
 fn successful_ping_pong() -> anyhow::Result<()> {
-    // Create a sender and mock env
-    let mock = MockBech32InterchainEnv::new("mock");
-    let env = PingPong::setup(mock)?;
+    logger_test_init();
+    let env = PingPong::setup()?;
     let app1 = env.app1;
+    let app2 = env.app2;
 
-    let pongs = app1.ping_pong()?;
-    assert_eq!(pongs, PongsResponse { pongs: 0 });
+    let r = app1.ping_pong(ChainName::from_chain_id(STARGAZE), 5)?;
     Ok(())
 }
--- modules/contracts/apps/ping-pong/src/contract.rs
+++ modules/contracts/apps/ping-pong/src/contract.rs
@@ -5,7 +5,10 @@ use crate::{
     msg::{AppExecuteMsg, AppInstantiateMsg, AppQueryMsg},
     replies::{self, INSTANTIATE_REPLY_ID},
 };
+use abstract_app::objects::dependency::StaticDependency;
+use abstract_app::std::IBC_CLIENT;
 use abstract_app::AppContract;
+
 use cosmwasm_std::Response;
 
 /// The version of your app
@@ -25,6 +28,10 @@ const APP: App = App::new(APP_ID, APP_VERSION, None)
     .with_query(handlers::query_handler)
     .with_migrate(handlers::migrate_handler)
     .with_replies(&[(INSTANTIATE_REPLY_ID, replies::instantiate_reply)])
+    .with_dependencies(&[StaticDependency::new(
+        IBC_CLIENT,
+        &[abstract_ibc_client::contract::CONTRACT_VERSION],
+    )])
     .with_module_ibc(crate::ibc::receive_module_ibc);
 
 // Export handlers
@@ -34,8 +41,23 @@ abstract_app::export_endpoints!(APP, App);
 abstract_app::cw_orch_interface!(APP, App, AppInterface);
 
 #[cfg(not(target_arch = "wasm32"))]
+use abstract_app::std::manager::ModuleInstallConfig;
+#[cfg(not(target_arch = "wasm32"))]
 impl<Chain: cw_orch::environment::CwEnv> abstract_app::abstract_interface::DependencyCreation
     for crate::AppInterface<Chain>
 {
     type DependenciesConfig = cosmwasm_std::Empty;
+
+    fn dependency_install_configs(
+        _configuration: Self::DependenciesConfig,
+    ) -> Result<Vec<ModuleInstallConfig>, abstract_app::abstract_interface::AbstractInterfaceError>
+    {
+        Ok(vec![ModuleInstallConfig::new(
+            abstract_app::objects::module::ModuleInfo::from_id(
+                IBC_CLIENT,
+                abstract_ibc_client::contract::CONTRACT_VERSION.into(),
+            )?,
+            None,
+        )])
+    }
 }
