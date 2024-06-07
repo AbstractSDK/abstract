b9c43849ff1370bc7afb0d8d91a9bbcbf9aad150
Kayanski <44806566+Kayanski@users.noreply.github.com>
2024-06-07 16:35:05 +0200

    Ibc Client installation goes through the usual install process (#362)
    * Revert "Revert "Fix automatic ibc client install" (#361)"
    
    This reverts commit 2e2e1c3bf483390837c0f3d03cd0d3be10e4f037.
    
    * nit
    
    ---------
    
    Co-authored-by: cyberhoward <cyberhoward@protonmail.com>
    Co-authored-by: CyberHoward <88450409+CyberHoward@users.noreply.github.com>

---
+++ framework/contracts/native/module-factory/tests/install_native_modules.rs
@@ -0,0 +1,48 @@
+use abstract_integration_tests::{create_default_account, AResult};
+use abstract_interface::{Abstract, AbstractAccount, Manager, ManagerQueryFns};
+use abstract_std::{
+    manager::{ExecuteMsg as ManagerMsg, ModuleInstallConfig},
+    objects::module::ModuleInfo,
+    IBC_CLIENT,
+};
+use anyhow::bail;
+use cw_orch::prelude::*;
+
+pub fn ibc_client_installed<Chain: CwEnv>(manager: &Manager<Chain>) -> AResult {
+    let ibc_addr = manager.module_addresses(vec![IBC_CLIENT.to_string()])?;
+    if ibc_addr.modules.is_empty() {
+        bail!("IBC client not installed")
+    }
+    Ok(())
+}
+
+#[test]
+fn can_install_and_uninstall_ibc_client() -> AResult {
+    let chain = MockBech32::new("mock");
+    let sender = chain.sender();
+    let abstr = Abstract::deploy_on(chain.clone(), sender.to_string())?;
+    let account = create_default_account(&abstr.account_factory)?;
+
+    let AbstractAccount { manager, proxy: _ } = &account;
+
+    ibc_client_installed(manager).unwrap_err();
+    manager.execute(
+        &ManagerMsg::InstallModules {
+            modules: vec![ModuleInstallConfig::new(
+                ModuleInfo::from_id_latest(IBC_CLIENT)?,
+                None,
+            )],
+        },
+        None,
+    )?;
+    ibc_client_installed(manager)?;
+    manager.execute(
+        &ManagerMsg::UninstallModule {
+            module_id: IBC_CLIENT.to_string(),
+        },
+        None,
+    )?;
+    ibc_client_installed(manager).unwrap_err();
+
+    Ok(())
+}
--- framework/packages/abstract-client/src/account.rs
+++ framework/packages/abstract-client/src/account.rs
@@ -40,7 +40,7 @@ use abstract_std::{
         AccountId, AssetEntry,
     },
     version_control::NamespaceResponse,
-    PROXY,
+    IBC_CLIENT, PROXY,
 };
 use cosmwasm_std::{to_json_binary, Attribute, Coins, CosmosMsg, Uint128};
 use cw_orch::{contract::Contract, environment::MutCwEnv, prelude::*};
@@ -549,7 +549,7 @@ impl<Chain: CwEnv> Account<Chain> {
 
     /// Set IBC status on an Account.
     pub fn set_ibc_status(&self, enabled: bool) -> AbstractClientResult<()> {
-        self.abstr_account.manager.update_settings(Some(enabled))?;
+        self.abstr_account.manager.set_ibc_status(enabled)?;
 
         Ok(())
     }
@@ -560,8 +560,16 @@ impl<Chain: CwEnv> Account<Chain> {
         host_chain: impl Into<String>,
         base_asset: Option<AssetEntry>,
         namespace: Option<String>,
-        install_modules: Vec<ModuleInstallConfig>,
+        mut install_modules: Vec<ModuleInstallConfig>,
     ) -> AbstractClientResult<<Chain as TxHandler>::Response> {
+        // We add the IBC Client by default in the modules installed on the remote account
+        if !install_modules.iter().any(|m| m.module.id() == IBC_CLIENT) {
+            install_modules.push(ModuleInstallConfig::new(
+                ModuleInfo::from_id_latest(IBC_CLIENT)?,
+                None,
+            ));
+        }
+
         self.abstr_account
             .manager
             .execute(
