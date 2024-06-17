88ae6d5d08643916017c55dbbaa8523b675bb4fd
Kayanski <kowalski.kowalskin@gmail.com>
2024-06-17 15:25:12 +0000

    Update script

--- framework/packages/abstract-interface/src/ibc.rs
+++ framework/packages/abstract-interface/src/ibc.rs
@@ -71,7 +71,6 @@ pub mod connection {
     use abstract_std::ibc_host::ExecuteMsgFns as _;
     use abstract_std::objects::chain_name::ChainName;
     use cw_orch_interchain::prelude::*;
-    use cw_orch_interchain::InterchainError;
     use cw_orch_polytone::Polytone;
     /// This is used for creating a testing connection between two Abstract connections using an existing polytone connection
     ///
@@ -81,13 +80,12 @@ pub mod connection {
         interchain: &IBC,
         dest: &Abstract<Chain>,
         polytone_src: &Polytone<Chain>,
-    ) -> Result<(), InterchainError> {
+    ) -> Result<(), AbstractInterfaceError> {
         // First we register client and host respectively
         let chain1_id = abstr.ibc.client.get_chain().chain_id();
-        let chain1_name = ChainName::from_chain_id(&chain1_id);
+        let chain1_name = ChainName::from_string(abstr.ibc.client.get_chain().env_info().chain_name)?;
 
-        let chain2_id = dest.ibc.client.get_chain().chain_id();
-        let chain2_name = ChainName::from_chain_id(&chain2_id);
+        let chain2_name = ChainName::from_string(dest.ibc.client.get_chain().env_info().chain_name)?;
 
         // First, we register the host with the client.
         // We register the polytone note with it because they are linked
