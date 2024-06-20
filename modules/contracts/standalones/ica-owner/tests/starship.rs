28961f5cf7718cd94951aee65fbc57debb0b59b4
Buckram <buckram123@gmail.com>
2024-06-20 10:37:09 +0300

    loadable test env

--- modules/contracts/standalones/ica-owner/tests/starship.rs
+++ modules/contracts/standalones/ica-owner/tests/starship.rs
@@ -1,22 +1,18 @@
-use cw_controllers::AdminError;
 use cw_ica_controller::types::msg::options::ChannelOpenInitOptions;
 use cw_orch_starship::Starship;
 use my_standalone::{
     ica_controller::ICAController,
-    msg::{
-        ConfigResponse, ICACountResponse, MyStandaloneExecuteMsgFns, MyStandaloneInstantiateMsg,
-        MyStandaloneQueryMsgFns,
-    },
-    MyStandaloneError, MyStandaloneInterface, MY_NAMESPACE,
+    msg::{MyStandaloneExecuteMsgFns, MyStandaloneInstantiateMsg, MyStandaloneQueryMsgFns},
+    MyStandaloneInterface, MY_NAMESPACE,
 };
 
-use abstract_client::{AbstractClient, Application, Environment};
+use abstract_client::{AbstractClient, Application};
 use abstract_standalone::{objects::namespace::Namespace, std::standalone};
 use cosmwasm_std::coins;
 // Use prelude to get all the necessary imports
 use cw_orch::{
     anyhow,
-    daemon::{json_lock::JsonLockedState, DaemonState, DaemonStateFile},
+    daemon::{json_lock::JsonLockedState, DaemonState},
     prelude::*,
 };
 use cw_orch_interchain::prelude::*;
@@ -30,11 +26,47 @@ struct TestEnv<Env: CwEnv> {
 }
 
 impl<Env: CwEnv> TestEnv<Env> {
+    fn load(src_env: Env, dst_env: Env) -> anyhow::Result<TestEnv<Env>> {
+        let ica_controller = ICAController::new(src_env.clone());
+        let ica_controller_code_id = ica_controller.code_id()?;
+
+        let abs_src = AbstractClient::new(src_env)?;
+        let abs_dst = AbstractClient::new(dst_env)?;
+
+        let namespace = Namespace::new(MY_NAMESPACE)?;
+        let publisher = abs_src.publisher_builder(namespace).build()?;
+
+        let sub_account = abs_src
+            .account_builder()
+            .sub_account(publisher.account())
+            .build()?;
+        let standalone = sub_account.install_standalone::<MyStandaloneInterface<_>>(
+            &MyStandaloneInstantiateMsg {
+                base: standalone::StandaloneInstantiateMsg {
+                    ans_host_address: abs_src.name_service().addr_str()?,
+                    version_control_address: abs_src.version_control().addr_str()?,
+                },
+                ica_controller_code_id,
+            },
+            &[],
+        )?;
+
+        Ok(TestEnv {
+            abs_src,
+            abs_dst,
+            standalone,
+            ica_controller,
+        })
+    }
+
     /// Set up the test environment with an Account that has the Standalone installed
     fn setup(src_env: Env, dst_env: Env) -> anyhow::Result<TestEnv<Env>> {
         let ica_controller = ICAController::new(src_env.clone());
-        let resp = ica_controller.upload()?;
-        let ica_controller_code_id = resp.uploaded_code_id()?;
+        if ica_controller.upload_if_needed()?.is_none() {
+            // If it's uploaded already just load
+            return Self::load(src_env, dst_env);
+        };
+        let ica_controller_code_id = ica_controller.code_id()?;
 
         let namespace = Namespace::new(MY_NAMESPACE)?;
 
@@ -71,7 +103,8 @@ impl<Env: CwEnv> TestEnv<Env> {
 
 // See https://github.com/AbstractSDK/cw-orchestrator/pull/424
 fn prepare_state_for_both_chains() {
-    let mut json_locked_state = JsonLockedState::new(&DaemonState::state_file_path().unwrap());
+    let state_file = DaemonState::state_file_path().unwrap();
+    let mut json_locked_state = JsonLockedState::new(&state_file);
 
     json_locked_state.prepare(&JUNO_1.chain_id, &JUNO_1.network_info.chain_name, "default");
     json_locked_state.prepare(
@@ -85,6 +118,7 @@ fn prepare_state_for_both_chains() {
 fn test_install() -> anyhow::Result<()> {
     std::env::set_var("RUST_LOG", "debug");
     // Make sure we don't corrupt actual state
+    // Don't forget to remove this file if it's fresh starship
     std::env::set_var(
         cw_orch::daemon::env::STATE_FILE_ENV_NAME,
         "starship-state.json",
