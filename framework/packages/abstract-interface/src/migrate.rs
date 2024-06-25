d7b2a9b8a3d1afce873d64d0e54cc971923e62d0
Buckram <buckram123@gmail.com>
2024-06-25 18:03:56 +0300

    migrate or deploy abstract ibc

--- framework/packages/abstract-interface/src/migrate.rs
+++ framework/packages/abstract-interface/src/migrate.rs
@@ -1,8 +1,8 @@
 use crate::{Abstract, AbstractIbc};
 use abstract_std::version_control::QueryMsgFns;
 use abstract_std::{
-    account_factory, ans_host, module_factory, objects::module::ModuleInfo, version_control,
-    MANAGER,
+    account_factory, ans_host, ibc_client, ibc_host, module_factory, objects::module::ModuleInfo,
+    version_control, MANAGER,
 };
 use abstract_std::{
     ACCOUNT_FACTORY, ANS_HOST, IBC_CLIENT, IBC_HOST, MODULE_FACTORY, PROXY, VERSION_CONTROL,
@@ -35,13 +35,17 @@ impl<T: CwEnv> Abstract<T> {
             .account
             .upload_and_register_if_needed(&self.version_control)?;
 
+        // Then ibc
+        let ibc = self.ibc.migrate_if_needed()?;
+
         self.version_control.approve_any_abstract_modules()?;
 
         Ok(account_factory.is_some()
             || module_factory.is_some()
             || version_control.is_some()
             || ans_host.is_some()
-            || account)
+            || account
+            || ibc)
     }
 
     /// Migrate the deployment based on version changes. If the registered contracts have the right version, we don't migrate them
@@ -141,8 +145,17 @@ impl<T: CwEnv> Abstract<T> {
             has_migrated = true
         }
 
-        if self.ibc.deploy_if_version_changed(self)? {
-            has_migrated = true
+        if self.ibc.deploy_or_migrate_if_version_changed(self)? {
+            has_migrated = true;
+
+            natives_to_register.push((
+                self.ibc.client.as_instance(),
+                ::ibc_client::contract::CONTRACT_VERSION.to_string(),
+            ));
+            natives_to_register.push((
+                self.ibc.host.as_instance(),
+                ::ibc_host::contract::CONTRACT_VERSION.to_string(),
+            ));
         }
 
         self.version_control.register_natives(natives_to_register)?;
@@ -261,25 +274,57 @@ fn contract_version<Chain: CwEnv, A: ContractInstance<Chain>>(
 }
 
 impl<Chain: CwEnv> AbstractIbc<Chain> {
-    /// Deploy the ibc based on version changes. If the registered contracts have the right version, we don't deploy them
-    pub fn deploy_if_version_changed(
+    /// Migrate the deployment based on the uploaded and local wasm files. If the remote wasm file is older, upload the contract and migrate to the new version.
+    pub fn migrate_if_needed(&self) -> Result<bool, crate::AbstractInterfaceError> {
+        let client = self
+            .client
+            .upload_and_migrate_if_needed(&ibc_client::MigrateMsg {})?;
+        let host = self
+            .host
+            .upload_and_migrate_if_needed(&ibc_host::MigrateMsg {})?;
+        Ok(client.is_some() || host.is_some())
+    }
+
+    /// Deploys or Migrates IBC Client and IBC Host
+    /// - If no version changes - don't do anything
+    /// - If version change is breaking - new version is deployed
+    /// - If version change is non-breaking - ibc contracts migrated instead
+    pub fn deploy_or_migrate_if_version_changed(
         &self,
         abstr: &Abstract<Chain>,
     ) -> Result<bool, crate::AbstractInterfaceError> {
-        // IBC client and IBC host supposed to be versioned equally
-        if ::ibc_client::contract::CONTRACT_VERSION == contract_version(&self.client)?.version {
-            // No need to deploy
+        let ibc_client_cw2_version = contract_version(&self.client)?.version;
+        // Check if any version changes
+        // *Note: IBC client and IBC host supposed to be versioned equally
+        if ::ibc_client::contract::CONTRACT_VERSION == ibc_client_cw2_version {
+            // No need to do anything
             return Ok(false);
         }
 
-        // Version changed we need to deploy new version
+        // Version change - upload both contracts
         self.client
             .upload_if_needed()?
             .expect("IBC client wasm might be outdated");
         self.host
             .upload_if_needed()?
             .expect("IBC host wasm might be outdated");
-        self.instantiate(abstr, &self.client.get_chain().sender())?;
+
+        // Check if version is breaking
+        let version_req = cw_semver::VersionReq::parse(&ibc_client_cw2_version).unwrap();
+        let new_version =
+            cw_semver::Version::parse(::ibc_client::contract::CONTRACT_VERSION).unwrap();
+        if version_req.matches(&new_version) {
+            // If version is not breaking, simply migrate
+            self.client
+                .migrate_if_needed(&ibc_client::MigrateMsg {})?
+                .expect("IBC client supposed to be migrated, but skipped instead");
+            self.host
+                .migrate_if_needed(&ibc_host::MigrateMsg {})?
+                .expect("IBC host supposed to be migrated, but skipped instead");
+        } else {
+            // Version change is breaking, need to deploy new version
+            self.instantiate(abstr, &self.client.get_chain().sender())?;
+        }
 
         Ok(true)
     }
