a82769e10244278c7e18ee6ee1ff7b15edb30fe3
Buckram <buckram123@gmail.com>
2024-08-09 17:50:27 +0300

    ensure only service obtainable

--- framework/packages/abstract-client/src/service.rs
+++ framework/packages/abstract-client/src/service.rs
@@ -1,25 +1,25 @@
-//! # Represents Abstract Application
+//! # Represents Abstract Service
 //!
-//! [`Application`] represents a module installed on a (sub-)account
+//! [`Service`] represents a module registered in version control
 
 use std::marker::PhantomData;
 
 use abstract_interface::{RegisteredModule, VersionControl};
-use abstract_std::objects::module::ModuleInfo;
+use abstract_std::objects::{module::ModuleInfo, module_reference::ModuleReference};
 use cw_orch::{contract::Contract, prelude::*};
 
 use crate::client::AbstractClientResult;
 
-/// An application represents a module installed on a (sub)-[`Account`].
+/// An service represents a module installed on a (sub)-[`Account`].
 ///
-/// It implements cw-orch traits of the module itself, so you can call its methods directly from the application struct.
+/// It implements cw-orch traits of the module itself, so you can call its methods directly from the service struct.
 #[derive(Clone)]
 pub struct Service<T: CwEnv, M> {
     module: M,
     chain: PhantomData<T>,
 }
 
-/// Allows to access the module's methods directly from the application struct
+/// Allows to access the module's methods directly from the service struct
 impl<Chain: CwEnv, M: InstantiableContract + ContractInstance<Chain>> InstantiableContract
     for Service<Chain, M>
 {
@@ -60,11 +60,18 @@ impl<Chain: CwEnv, M: RegisteredModule> Service<Chain, M> {
         version_control: &VersionControl<Chain>,
         module: M,
     ) -> AbstractClientResult<Self> {
-        // Sanity check: the module must be in version control
-        version_control.module(ModuleInfo::from_id(
-            M::module_id(),
-            abstract_std::objects::module::ModuleVersion::Version(M::module_version().to_owned()),
-        )?)?;
+        // Sanity check: the module must be in version control and service
+        let module_reference: ModuleReference = version_control
+            .module(ModuleInfo::from_id(
+                M::module_id(),
+                abstract_std::objects::module::ModuleVersion::Version(
+                    M::module_version().to_owned(),
+                ),
+            )?)?
+            .reference;
+        if !matches!(module_reference, ModuleReference::Service(_)) {
+            return Err(crate::AbstractClientError::ExpectedService {});
+        }
         Ok(Self {
             module,
             chain: PhantomData {},
