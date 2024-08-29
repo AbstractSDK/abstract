7fab9b88b13c3d2a0ac9374f686d43605071e559
cyberhoward <cyberhoward@protonmail.com>
2024-08-29 12:22:26 +0200

    rename dir

--- framework/packages/abstract-std/src/core/merged.rs
+++ framework/packages/abstract-std/src/core/merged.rs
@@ -1,9 +1,25 @@
+use manager::ModuleInstallConfig;
+
+use crate::objects::{gov_type::GovernanceDetails, AccountId};
+
 use super::*;
 
+
+/// Account Instantiate Msg
+/// https://github.com/burnt-labs/contracts/blob/main/contracts/account/src/msg.rs
 #[cosmwasm_schema::cw_serde]
-pub struct InitMsg {
-    pub manager: manager::InstantiateMsg,
-    pub proxy: proxy::InstantiateMsg,
+pub struct InstantiateMsg {
+    // TODO: fork and make pub
+    // pub authenticator: AddAuthenticator,
+    pub account_id: AccountId,
+    // Optionally modules can be provided. They will be installed after account registration.
+    pub install_modules: Vec<ModuleInstallConfig>,
+    pub name: String,
+    pub description: Option<String>,
+    pub link: Option<String>,
+    pub module_factory_address: String,
+    pub version_control_address: String,
+    pub ans_host_address: String,
 }
 
 #[cosmwasm_schema::cw_serde]
