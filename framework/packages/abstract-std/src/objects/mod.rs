6576f62483a250fe8ec05f2869da0ed9e043a26d
Mykhailo Donchenko <91957742+Buckram123@users.noreply.github.com>
2024-09-16 16:26:33 +0300

    Move `abstract-std::objects::account.rs` content to where it belongs (#453)
    * move account.rs to module in objects/mod.rs
    
    * move ACCOUNT_ID item to account state

--- framework/packages/abstract-std/src/objects/mod.rs
+++ framework/packages/abstract-std/src/objects/mod.rs
@@ -12,7 +12,6 @@ pub mod salt;
 
 pub use pool::*;
 
-pub mod account;
 pub mod dependency;
 pub mod deposit_info;
 pub mod deposit_manager;
@@ -29,7 +28,7 @@ pub(crate) mod truncated_chain_id;
 pub mod validation;
 pub mod voting;
 
-pub use account::{AccountId, ABSTRACT_ACCOUNT_ID};
+pub use account::{AccountId, AccountSequence, AccountTrace, ABSTRACT_ACCOUNT_ID};
 pub use ans_asset::AnsAsset;
 pub use entry::{
     ans_entry_convertor::AnsEntryConvertor,
@@ -47,4 +46,15 @@ pub mod chain_name {
     // Type name `ChainName` was not suitable name for the type
     #[deprecated = "Use TruncatedChainId instead"]
     pub type ChainName = TruncatedChainId;
+}
+
+pub mod account {
+    mod account_id;
+    mod account_trace;
+
+    pub use self::{account_id::AccountId, account_trace::AccountTrace};
+
+    pub const ABSTRACT_ACCOUNT_ID: AccountId = AccountId::const_new(0, AccountTrace::Local);
+
+    pub type AccountSequence = u32;
 }
