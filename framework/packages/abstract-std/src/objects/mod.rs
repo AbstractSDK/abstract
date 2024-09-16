0ecf2467d040b135b3571965ef5816787c0b0fc5
Buckram <buckram123@gmail.com>
2024-09-16 16:24:37 +0300

    move ACCOUNT_ID item to account state

--- framework/packages/abstract-std/src/objects/mod.rs
+++ framework/packages/abstract-std/src/objects/mod.rs
@@ -52,14 +52,9 @@ pub mod account {
     mod account_id;
     mod account_trace;
 
-    use cw_storage_plus::Item;
-
     pub use self::{account_id::AccountId, account_trace::AccountTrace};
 
     pub const ABSTRACT_ACCOUNT_ID: AccountId = AccountId::const_new(0, AccountTrace::Local);
 
     pub type AccountSequence = u32;
-
-    /// Account Id storage key
-    pub const ACCOUNT_ID: Item<AccountId> = Item::new("acc_id");
 }
