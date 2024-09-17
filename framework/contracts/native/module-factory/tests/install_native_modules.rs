14eaef3fdd680a037ac1fe5dbb72f5443e0d1f62
Buckram <buckram123@gmail.com>
2024-09-17 15:15:03 +0300

    module-factory tests

--- framework/contracts/native/module-factory/tests/install_native_modules.rs
+++ framework/contracts/native/module-factory/tests/install_native_modules.rs
@@ -1,15 +1,15 @@
 use abstract_integration_tests::{create_default_account, AResult};
-use abstract_interface::{Abstract, AbstractAccount, Account, ManagerQueryFns};
+use abstract_interface::{Abstract, AccountI, AccountQueryFns};
 use abstract_std::{
-    manager::{ExecuteMsg as ManagerMsg, ModuleInstallConfig},
+    account::{ExecuteMsg as AccountMsg, ModuleInstallConfig},
     objects::module::ModuleInfo,
     IBC_CLIENT,
 };
 use anyhow::bail;
 use cw_orch::prelude::*;
 
-pub fn ibc_client_installed<Chain: CwEnv>(manager: &Account<Chain>) -> AResult {
-    let ibc_addr = manager.module_addresses(vec![IBC_CLIENT.to_string()])?;
+pub fn ibc_client_installed<Chain: CwEnv>(account: &AccountI<Chain>) -> AResult {
+    let ibc_addr = account.module_addresses(vec![IBC_CLIENT.to_string()])?;
     if ibc_addr.modules.is_empty() {
         bail!("IBC client not installed")
     }
@@ -21,16 +21,11 @@ fn can_install_and_uninstall_ibc_client() -> AResult {
     let chain = MockBech32::new("mock");
     let sender = chain.sender_addr();
     let abstr = Abstract::deploy_on(chain.clone(), sender.to_string())?;
-    let account = create_default_account(&abstr.account_factory)?;
+    let account = create_default_account(&chain.sender_addr(), &abstr)?;
 
-    let AbstractAccount {
-        account: manager,
-        proxy: _,
-    } = &account;
-
-    ibc_client_installed(manager).unwrap_err();
-    manager.execute(
-        &ManagerMsg::InstallModules {
+    ibc_client_installed(&account).unwrap_err();
+    account.execute(
+        &AccountMsg::InstallModules {
             modules: vec![ModuleInstallConfig::new(
                 ModuleInfo::from_id_latest(IBC_CLIENT)?,
                 None,
@@ -38,14 +33,14 @@ fn can_install_and_uninstall_ibc_client() -> AResult {
         },
         &[],
     )?;
-    ibc_client_installed(manager)?;
-    manager.execute(
-        &ManagerMsg::UninstallModule {
+    ibc_client_installed(&account)?;
+    account.execute(
+        &AccountMsg::UninstallModule {
             module_id: IBC_CLIENT.to_string(),
         },
         &[],
     )?;
-    ibc_client_installed(manager).unwrap_err();
+    ibc_client_installed(&account).unwrap_err();
 
     Ok(())
 }
