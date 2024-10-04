3405659f83f57730ed56c2691a66a0148df4bd43
Kayanski <kowalski.kowalskin@gmail.com>
2024-10-04 09:01:01 +0000

    Updated cw-orch

--- framework/contracts/native/ica-client/examples/integration_test.rs
+++ framework/contracts/native/ica-client/examples/integration_test.rs
@@ -5,4 +5,14 @@ fn main() -> anyhow::Result<()> {
 
     let chain = Daemon::builder().chain(UNION_TESTNET_8).build()?;
     let abs = Abstract::load_from(chain.clone())?;
+
+    // We get the account and install the ICA client app on it
+
+    // We start by sending some funds to the interchain account to be able to send it around in the ica action
+
+    // We query the ICA client action from the script
+
+    // We send the message from the account directly
+
+    // We make sure the messages do the right actions with a query on the EVM chain
 }
