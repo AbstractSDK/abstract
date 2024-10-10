a2a1a7eaa88ae0ef0db33aa84edf6a925cb7c138
Kayanski <kowalski.kowalskin@gmail.com>
2024-10-10 12:48:56 +0000

    Working full script with ica client

--- interchain/interchain-tests/src/bin/ica_client.rs
+++ interchain/interchain-tests/src/bin/ica_client.rs
@@ -2,7 +2,7 @@ use abstract_client::{AbstractClient, Namespace};
 use abstract_ica_client::chain_types::evm::types::{UCS01_FORWARDER_CONTRACT, UCS01_PROTOCOL};
 use abstract_interface::{ExecuteMsgFns, IcaClient};
 use abstract_std::{
-    ica_client::{IcaAction, IcaActionResult, InstantiateMsg, QueryMsg, QueryMsgFns},
+    ica_client::{IcaAction, IcaActionResult, IcaExecute, InstantiateMsg, QueryMsg, QueryMsgFns},
     objects::{
         namespace::ABSTRACT_NAMESPACE, ContractEntry, UncheckedChannelEntry, UncheckedContractEntry,
     },
@@ -12,12 +12,23 @@ use alloy::{
     primitives::{Address, Uint},
     providers::Provider,
 };
-use cosmwasm_std::{coins, Binary};
+use alloy::{
+    providers::RootProvider,
+    sol_types::SolCall,
+    transports::http::{Client, Http},
+};
+use cosmwasm_std::{coin, Binary, HexBinary};
 use cw_orch::prelude::*;
+use cw_orch_interchain::prelude::PacketAnalysis;
 use cw_orch_interchain::{core::IbcQueryHandler, prelude::InterchainEnv};
-use evm_note::interface::EvmNote;
+use evm_note::msg::QueryMsgFns as _;
+use evm_note::{interface::EvmNote, msg::EvmMsg};
 use networks::union::UNION_TESTNET_8;
-use polytone_evm::bind::ierc20::ERC20;
+use polytone_evm::bind::{
+    evmvoice::EvmVoice::{self, Sender},
+    ierc20::ERC20,
+};
+use queriers::{Ibc, Node};
 use std::future::IntoFuture;
 use union_connector::{interchain_env::UnionInterchainEnv, networks::UncheckedRemoteEvmConfig};
 
@@ -30,49 +41,43 @@ fn main() -> cw_orch::anyhow::Result<()> {
     pretty_env_logger::init();
     // This is an integration test with Abstract And polytone EVM already deployed on Union
 
-    // If it's not deployed, we can redeploy it here
     let chain_info = UNION_TESTNET_8;
-
     let chain = Daemon::builder(chain_info.clone()).build()?;
 
+    // If it's not deployed, we can redeploy it here
     // let abs = AbstractClient::builder(chain.clone()).build(chain.sender().clone())?;
     let abs = AbstractClient::new(chain.clone())?;
 
-    // We get the account and install the ICA client app on it
+    // We get the account
     let account = abs
         .account_builder()
         .namespace(Namespace::new(TEST_ACCOUNT_NAMESPACE)?)
         .build()?;
-    // Install IBC if not installed
-    if !account.module_installed(IBC_CLIENT)? {
-        account
-            .as_ref()
-            .install_module::<Empty>(IBC_CLIENT, None, &[])?;
-    }
 
+    let account_coins = coin(9, chain_info.gas_denom);
     // We start by sending some funds to the interchain account to be able to send it around in the ica action
-    let account_balance = account.query_balance(chain_info.gas_denom)?;
-    let account_coins = coins(9, chain_info.gas_denom);
-    if account_balance < account_coins[0].amount {
-        log::warn!("Sending some funds from wallet to account.");
-        // @feedback make it easier to send funds from wallet?
-        //  - maybe     .deposit() method
-        chain.rt_handle.block_on(chain.sender().bank_send(
-            // @feedback: test_acc.address() to get the address of the proxy?
-            &account.address()?,
-            account_coins.clone(),
-        ))?;
+    {
+        let account_balance = account.query_balance(chain_info.gas_denom)?;
+        if account_balance < account_coins.amount {
+            log::warn!("Sending some funds from wallet to account.");
+            // @feedback make it easier to send funds from wallet?
+            //  - maybe     .deposit() method
+            chain.rt_handle.block_on(chain.sender().bank_send(
+                // @feedback: test_acc.address() to get the address of the proxy?
+                &account.address()?,
+                vec![account_coins.clone()],
+            ))?;
+        }
     }
 
     // We need to register the EVM note here (already existent in state.json)
     // abs.registry().register_natives(vec![(
-    //     EvmNote::new(chain.clone()).as_instance(),
+    //     evm_note.as_instance(),
     //     evm_note::contract::CONTRACT_VERSION.to_string(),
     // )])?;
 
     // We need to register the IBC channels
 
-    let evm_config = union_connector::networks::get_remote_evm_config(CHAIN_NAME).unwrap();
     // abs.name_service().update_channels(
     //     vec![
     //         (
@@ -100,33 +105,109 @@ fn main() -> cw_orch::anyhow::Result<()> {
     //     vec![],
     // )?;
 
-    // We query the ICA client action from the script
-    let receiver_address = "76FaA72D2949072f24251f4D84cDCb60265d6697";
-
+    let evm_config = union_connector::networks::get_remote_evm_config(CHAIN_NAME).unwrap();
+    let evm_note = EvmNote::new(chain.clone());
     let interchain = UnionInterchainEnv::new(chain.clone(), &evm_config);
-    let funds_before = get_balance(&interchain, &evm_config, receiver_address)?;
 
-    // We send the message from the account directly
-    let ica_action = abs.ica_client().ica_action(
-        account.address()?.to_string(),
-        vec![IcaAction::Fund {
-            funds: account_coins,
-            receiver: Some(hex::decode(receiver_address)?.into()),
-            memo: None,
-        }],
-        "bartio".parse()?,
+    let remote_address = get_remote_address(
+        &evm_note,
+        &interchain,
+        &evm_config,
+        account.address()?.as_str(),
     )?;
 
-    let tx_response = account.execute(ica_action.msgs, &[])?;
+    log::info!(
+        "Sending funds to the remote address on {CHAIN_NAME}: {:?}",
+        remote_address
+    );
 
-    // We make sure the messages do the right actions with a query on the EVM chain
-    interchain.await_and_check_packets(&chain.chain_id(), tx_response.into())?;
-    // let tx_hash = "66F33B8A09DFCD8079B110BD84D58D2644D19018C2F9B53789B521A4FA091D3D";
-    // interchain.await_from_tx_hash(&chain.chain_id(), tx_hash.to_string())?;
+    let funds_before = get_balance(&interchain, &evm_config, &remote_address)?;
 
-    let funds_after = get_balance(&interchain, &evm_config, receiver_address)?;
+    // We send some funds from the cosmos chain to the remote address
+    let funds_after = {
+        let ica_action = abs.ica_client().ica_action(
+            account.address()?.to_string(),
+            vec![IcaAction::Fund {
+                funds: vec![account_coins.clone()],
+                receiver: Some(remote_address.as_slice().into()),
+                memo: None,
+            }],
+            "bartio".parse()?,
+        )?;
 
-    assert_eq!((funds_after - funds_before).to_string(), 9.to_string());
+        // In case we want to execute the transaction
+        let tx_response = account.execute(ica_action.msgs, &[])?;
+        interchain.await_and_check_packets(&chain.chain_id(), tx_response.into())?;
+        let funds_after = get_balance(&interchain, &evm_config, &remote_address)?;
+        assert_eq!((funds_after - funds_before).to_string(), 9.to_string());
+        funds_after
+
+        // In case we want to debug and the transaction has already been executed
+        // let tx_hash = "012BD18DDDEA3E73193AF2172B1958417245D20EB7E16A394260893F3C4149F9";
+        // interchain
+        //     .await_from_tx_hash(&chain.chain_id(), tx_hash.to_string())?
+        //     .assert()?;
+        // get_balance(&interchain, &evm_config, &remote_address)?
+    };
+
+    // We want to send the funds back now from the address to the local address
+    {
+        let muno_erc20_addr = evm_config.muno_erc20.unwrap();
+
+        let approve_msg = EvmMsg::call(
+            muno_erc20_addr.to_string(),
+            HexBinary::from(
+                ERC20::approveCall::new((
+                    Address::parse_checksummed(evm_config.ucs01_handler, None)?,
+                    alloy::primitives::U256::from(3),
+                ))
+                .abi_encode(),
+            ),
+        );
+        let send_back_msg = EvmMsg::call(
+            evm_config.ucs01_handler.to_string(),
+            HexBinary::from(
+                polytone_evm::bind::irelay::IRelay::sendCall::new(
+                    // srcChannel: channel-90 - receiver: 0x3d95c07a0380cff70fb9d086f076e19a8a3807cb - denom: 0xfFf9976782d46CC05630D1f6eBAb18b2324d6B14 - amount: 50000000000000 - extension:  - revNumber: 9 - revHeight: 1000000099 - timeStamp: 0
+                    (
+                        evm_config.ics20_src_channel.to_string(),
+                        bech32::decode(&account.address()?.to_string())?.1.into(),
+                        vec![polytone_evm::bind::irelay::IRelay::LocalToken {
+                            denom: Address::parse_checksummed(muno_erc20_addr, None)?,
+                            amount: account_coins.amount.u128(),
+                        }],
+                        "".to_string(),
+                        polytone_evm::bind::irelay::IRelay::IbcCoreClientV1HeightData {
+                            revision_number: 9,
+                            revision_height: 1000000099,
+                        },
+                        0,
+                    ),
+                )
+                .abi_encode(),
+            ),
+        );
+
+        let ica_action = abs.ica_client().ica_action(
+            account.address()?.to_string(),
+            vec![IcaAction::Execute(IcaExecute::Evm {
+                msgs: vec![approve_msg, send_back_msg],
+                callback: None,
+            })],
+            CHAIN_NAME.parse()?,
+        )?;
+
+        let tx_response = account.execute(ica_action.msgs, &[])?;
+        interchain.await_and_check_packets(&chain.chain_id(), tx_response.into())?;
+        let funds_final = get_balance(&interchain, &evm_config, &remote_address)?;
+        assert_eq!((funds_after - funds_final).to_string(), 9.to_string());
+
+        // In case we want to debug and the transaction has already been executed
+        // let tx_hash = "827026275B6931567810D471ADCE9779D0B4FE9AC57F50783EAAADE16E6553F4";
+        // interchain
+        //     .await_from_tx_hash(&evm_config.chain_id().to_string(), tx_hash.to_string())?
+        //     .assert()?;
+    }
 
     Ok(())
 }
@@ -134,7 +215,7 @@ fn main() -> cw_orch::anyhow::Result<()> {
 fn get_balance(
     interchain: &UnionInterchainEnv,
     evm_config: &UncheckedRemoteEvmConfig,
-    address: &str,
+    address: &Address,
 ) -> anyhow::Result<Uint<256, 4>> {
     let evm = interchain.get_evm_chain(evm_config.chain_id)?;
     let muno_erc20_addr = evm_config.muno_erc20.unwrap();
@@ -142,10 +223,110 @@ fn get_balance(
         Address::parse_checksummed(muno_erc20_addr, None)?,
         evm.clone(),
     );
-    let remote_address: Address = address.parse()?;
     let remote_muno_balance = interchain
         .rt
-        .block_on(muno_erc20.balanceOf(remote_address).call().into_future())?;
+        .block_on(muno_erc20.balanceOf(*address).call().into_future())?;
 
     Ok(remote_muno_balance._0)
+}
+
+// let evm= EvmVoice::new(evm_note_address, evm);
+
+fn predict_remote_address(
+    cosmos_note: &EvmNote<Daemon>,
+    interchain: UnionInterchainEnv,
+    evm_config: &UncheckedRemoteEvmConfig,
+    sender_address: &str,
+) -> anyhow::Result<Address> {
+    let rt = &cosmos_note.environment().rt_handle;
+    let evm_voice = {
+        let voice_address = cosmos_note.pair()?.unwrap().remote_port;
+        let evm = interchain.get_evm_chain(evm_config.chain_id)?;
+        EvmVoice::new(voice_address.parse()?, evm)
+    };
+    let source_port = format!("wasm.{}", cosmos_note.addr_str()?);
+    let connection = {
+        let source_channel = cosmos_note.active_channel()?.unwrap();
+        let node: Ibc = cosmos_note.environment().querier();
+        let active_channel = rt.block_on(node._channel(source_port.clone(), source_channel))?;
+        let destination_channel = active_channel.counterparty.unwrap().channel_id;
+        rt.block_on(
+            evm_voice
+                .channelToConnection(destination_channel.to_string())
+                .call()
+                .into_future(),
+        )?
+        ._0
+    };
+
+    let sender = Sender {
+        connection: connection.to_string(),
+        port: source_port.to_string(),
+        sender: sender_address.to_string(),
+    };
+    Ok(rt
+        .block_on(
+            evm_voice
+                .getExpectedProxyAddress(sender.clone())
+                .call()
+                .into_future(),
+        )?
+        ._0)
+}
+
+fn get_remote_address(
+    cosmos_note: &EvmNote<Daemon>,
+    interchain: &UnionInterchainEnv,
+    evm_config: &UncheckedRemoteEvmConfig,
+    sender_address: &str,
+) -> anyhow::Result<Address> {
+    let rt = &cosmos_note.environment().rt_handle;
+    let evm_voice = {
+        let voice_address = cosmos_note.pair()?.unwrap().remote_port;
+        let evm = interchain.get_evm_chain(evm_config.chain_id)?;
+        EvmVoice::new(voice_address.parse()?, evm)
+    };
+    let source_port = format!("wasm.{}", cosmos_note.addr_str()?);
+    let connection = {
+        let source_channel = cosmos_note.active_channel()?.unwrap();
+        let node: Ibc = cosmos_note.environment().querier();
+        let active_channel = rt.block_on(node._channel(source_port.clone(), source_channel))?;
+        let destination_channel = active_channel.counterparty.unwrap().channel_id;
+        rt.block_on(
+            evm_voice
+                .channelToConnection(destination_channel.to_string())
+                .call()
+                .into_future(),
+        )?
+        ._0
+    };
+
+    let sender = Sender {
+        connection: connection.to_string(),
+        port: source_port.to_string(),
+        sender: sender_address.to_string(),
+    };
+    let proxy_address = rt
+        .block_on(
+            evm_voice
+                .getProxyAddress(sender.clone())
+                .call()
+                .into_future(),
+        )?
+        ._0;
+    if proxy_address != Address::ZERO {
+        Ok(proxy_address)
+    } else {
+        // In case no proxy address is registered, we fetch it !
+        let proxy_address = rt
+            .block_on(
+                evm_voice
+                    .getExpectedProxyAddress(sender.clone())
+                    .call()
+                    .into_future(),
+            )?
+            ._0;
+
+        Ok(proxy_address)
+    }
 }
