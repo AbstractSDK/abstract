b8af662ca65c7e0427a09d8f4ab141e71ce85426
Buckram <buckram123@gmail.com>
2024-10-16 13:16:51 +0300

    missed custom getrandom

--- framework/packages/abstract-xion/src/lib.rs
+++ framework/packages/abstract-xion/src/lib.rs
@@ -43,3 +43,14 @@ pub enum AccountSudoMsg {
         simulate: bool,
     },
 }
+
+// the random function must be disabled in cosmwasm
+use core::num::NonZeroU32;
+use getrandom::Error;
+
+pub fn always_fail(_buf: &mut [u8]) -> Result<(), Error> {
+    let code = NonZeroU32::new(Error::CUSTOM_START).unwrap();
+    Err(Error::from(code))
+}
+use getrandom::register_custom_getrandom;
+register_custom_getrandom!(always_fail);
