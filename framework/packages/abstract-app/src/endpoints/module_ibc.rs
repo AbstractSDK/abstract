e1ad44547563985b2830567484ff77a6b956bbab
Buckram <buckram123@gmail.com>
2024-11-04 15:55:26 +0200

    remove instantiation from migration for ibc-client and ibc-host

--- framework/packages/abstract-app/src/endpoints/module_ibc.rs
+++ framework/packages/abstract-app/src/endpoints/module_ibc.rs
@@ -36,26 +36,3 @@ impl<
         Ok(registry_query_result.reference.unwrap_native()?)
     }
 }
-
-#[cfg(test)]
-mod test {
-    use abstract_sdk::base::ModuleIbcEndpoint;
-    use abstract_std::native_addrs;
-    use abstract_testing::mock_env_validated;
-    use cosmwasm_std::Api;
-
-    use crate::mock::{mock_init, BASIC_MOCK_APP};
-
-    #[coverage_helper::test]
-    fn ibc_host_address() {
-        let deps = mock_init();
-        let env = mock_env_validated(deps.api);
-        let ibc_host = BASIC_MOCK_APP.ibc_host(deps.as_ref(), &env);
-
-        let hrp = native_addrs::hrp_from_env(&env);
-        let expected_ibc_host_canon = native_addrs::ibc_host_address(hrp, &deps.api).unwrap();
-        let expected_ibc_host = deps.api.addr_humanize(&expected_ibc_host_canon).unwrap();
-
-        assert_eq!(ibc_host, Ok(expected_ibc_host))
-    }
-}
