5c4486a3cf8378aab3b28740a2e7bd820fe39ee7
Kayanski <kowalski.kowalskin@gmail.com>
2024-11-26 15:29:16 +0000

    rEMOVE TESTING for oracle

--- framework/packages/standards/oracle/src/lib.rs
+++ framework/packages/standards/oracle/src/lib.rs
@@ -2,8 +2,6 @@ mod command;
 mod error;
 
 pub mod msg;
-#[cfg(feature = "testing")]
-pub mod tests;
 
 // Export interface for use in SDK modules
 pub use abstract_adapter_utils::{coins_in_assets, cw_approve_msgs, Identify};
