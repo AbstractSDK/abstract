34cd097ea65212045c6d469d04579f5c85402c7f
Kayanski <kowalski.kowalskin@gmail.com>
2024-12-02 11:13:27 +0000

    Added payment app + corrected local chain

--- integrations/neutron-dex-adapter/src/lib.rs
+++ integrations/neutron-dex-adapter/src/lib.rs
@@ -1,6 +1,6 @@
 pub const NEUTRON: &str = "neutron";
 #[cfg(feature = "local")]
-pub const AVAILABLE_CHAINS: &[&str] = abstract_sdk::std::registry::LOCAL_CHAIN;
+pub const AVAILABLE_CHAINS: &[&str] = abstract_sdk::std::constants::LOCAL_CHAIN;
 #[cfg(not(feature = "local"))]
 pub const AVAILABLE_CHAINS: &[&str] = abstract_sdk::std::constants::NEUTRON;
 
